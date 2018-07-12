36cb236ca2570aaea632a8e37b3637c53c7e4bd5
ned <daniele.nerini@meteoswiss.ch>
2018-07-12 11:35:25 +0200

    Fix interface to importers

--- pysteps/io/readers.py
+++ pysteps/io/readers.py
@@ -18,12 +18,17 @@ def read_timeseries(inputfns, importer, **kwargs):
     Returns
     -------
     out : tuple
-        A three-element tuple containing the precipitation fields read and 
-        the associated georeferencing data and metadata.
+        A three-element tuple containing the precipitation fields read, the quality field,
+        and associated metadata.
     """
     R = []
+    Q = []
     for ifn in inputfns[0]:
-        R_, geodata, metadata = importer(ifn, **kwargs)
+        R_, Q_, metadata = importer(ifn, **kwargs)
         R.append(R_)
+        Q.append(Q_)
+        
+    R = np.concatenate([R_[None, :, :] for R_ in R])    
+    Q = np.concatenate([Q_[None, :, :] for Q_ in Q])    
 
-    return np.concatenate([R_[None, :, :] for R_ in R]), geodata, metadata
+    return R, Q, metadata
