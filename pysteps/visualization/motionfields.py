fa4d42a05b319ef5867b2067abb698a1d661d738
ned <daniele.nerini@meteoswiss.ch>
2018-07-16 11:10:52 +0200

    Fix orientation of y-axis

--- pysteps/visualization/motionfields.py
+++ pysteps/visualization/motionfields.py
@@ -5,16 +5,28 @@ import matplotlib.colors as colors
 
 import numpy as np
 
-def quiver(UV, geodata=None, step=15):
-    """Function to plot a precipitation field witha a colorbar. 
+def quiver(UV, metadata=None, step=20):
+    """Function to plot a motion field as arrows. 
     
     Parameters 
     ---------- 
     UV : array-like 
         Array of shape (2, m,n) containing the input motion field.
-    geodata : dictionary
+    metadata : dictionary
+        Optional dictionary containing geographical information about the field. 
+        If metadata is not None, it must contain the following key-value pairs:
+        
+        x1           x-coordinate of the lower-left corner of the data raster (meters)
+        y1           y-coordinate of the lower-left corner of the data raster (meters)
+        x2           x-coordinate of the upper-right corner of the data raster (meters)
+        y2           y-coordinate of the upper-right corner of the data raster (meters)
+        yorigin      a string specifying the location of the first element in
+                     the data raster w.r.t. y-axis:
+                     'upper' = upper border
+                     'lower' = lower border
     step : int
-    
+        Optional resample step to reduce the density of the arrows.
+        
     Returns: 
     ----------
     ax : fig axes
@@ -22,39 +34,48 @@ def quiver(UV, geodata=None, step=15):
     
     """
     
-    # prepare X Y coordinates
-    if geodata is not None:
-        X,Y = np.meshgrid(np.arange(geodata['x1']/1000,geodata['x2']/1000),
-                          np.arange(geodata['y1']/1000,geodata['y2']/1000))
+    # prepare x y coordinates
+    if metadata is not None:
+        x = np.arange(metadata['x1']/1000, metadata['x2']/1000)
+        y = np.arange(metadata['y1']/1000, metadata['y2']/1000)
     else:
-        X,Y = np.meshgrid(np.arange(UV.shape[2]),
-                          np.arange(UV.shape[1]))
+        x = np.arange(UV.shape[2])
+        y = np.arange(UV.shape[1])
     
     # reduce number of vectors to plot
     UV_ = UV[:, 0:UV.shape[1]:step, 0:UV.shape[2]:step]
-    X_ = X[0:UV.shape[1]:step, 0:UV.shape[2]:step]
-    Y_ = Y[0:UV.shape[1]:step, 0:UV.shape[2]:step]
+    y_ = y[0:UV.shape[1]:step]
+    x_ = x[0:UV.shape[2]:step]
     
-    plt.quiver(X_, Y_, UV_[0,:,:], -UV_[1,:,:], pivot='tip')
-    
+    plt.quiver(x_, np.flipud(y_), UV_[0,:,:], -UV_[1,:,:], angles='xy')
+        
     axes = plt.gca()
     
-    if geodata is None:
+    if metadata is None:
         axes.xaxis.set_ticklabels([])
         axes.yaxis.set_ticklabels([])
    
     return axes
     
-def streamplot(UV, geodata=None):
-    """Function to plot a precipitation field witha a colorbar. 
+def streamplot(UV, metadata=None):
+    """Function to plot a motion field as streamlines. 
     
     Parameters 
     ---------- 
     UV : array-like 
         Array of shape (2, m,n) containing the input motion field.
-    geodata : dictionary
+    metadata : dictionary
+        Optional dictionary containing geographical information about the field. 
+        If metadata is not None, it must contain the following key-value pairs:
         
-    step : int
+        x1           x-coordinate of the lower-left corner of the data raster (meters)
+        y1           y-coordinate of the lower-left corner of the data raster (meters)
+        x2           x-coordinate of the upper-right corner of the data raster (meters)
+        y2           y-coordinate of the upper-right corner of the data raster (meters)
+        yorigin      a string specifying the location of the first element in
+                     the data raster w.r.t. y-axis:
+                     'upper' = upper border
+                     'lower' = lower border
     
     Returns: 
     ----------
@@ -63,19 +84,19 @@ def streamplot(UV, geodata=None):
     
     """
     
-    # prepare X Y coordinates
-    if geodata is not None:
-        x = np.arange(geodata['x1']/1000,geodata['x2']/1000)
-        y = np.arange(geodata['y1']/1000,geodata['y2']/1000)
+    # prepare x y coordinates
+    if metadata is not None:
+        x = np.arange(metadata['x1']/1000,metadata['x2']/1000)
+        y = np.arange(metadata['y1']/1000,metadata['y2']/1000)
     else:
         x = np.arange(UV.shape[2])
-        y = np.arange(UV.shape[1])
+        y = np.arange(UV.shape[1],0,-1)
         
-    plt.streamplot(x, y, UV[0,:,:], -UV[1,:,:], density=2, color='black')
+    plt.streamplot(x, np.flipud(y), UV[0,:,:], -UV[1,:,:], density=1.5, color='black')
     
     axes = plt.gca()
     
-    if geodata is None:
+    if metadata is None:
         axes.xaxis.set_ticklabels([])
         axes.yaxis.set_ticklabels([])
    
