1684993d0dde53680794f69f0009ca649f670516
Seppo Pulkkinen <pulkkins@gmail.com>
2018-07-25 18:38:14 +0200

    Add alternative implementation of probability matching

--- pysteps/postproc/probmatching.py
+++ pysteps/postproc/probmatching.py
@@ -1,6 +1,20 @@
 ''' Methods for probability matching'''
 
 import numpy as np
+from scipy import interpolate as sip
+
+def compute_empirical_cdf(bin_edges, hist):
+    cdf = []
+    xs = 0.0
+    
+    for x,h in zip(zip(bin_edges[:-1], bin_edges[1:]), hist):
+        cdf.append(xs)
+        xs += (x[1] - x[0]) * h
+    
+    cdf.append(xs)
+    cdf = np.array(cdf) / xs
+    
+    return cdf
 
 def nonparam_match_empirical_cdf(initialarray, targetarray):
     """Matches the empirical CDF of the initial array with the empirical CDF
@@ -53,3 +67,39 @@ def nonparam_match_empirical_cdf(initialarray, targetarray):
     outputarray[idxzeros] = zvalue
 
     return outputarray
+
+def nonparam_match_empirical_cdf_masked():
+    pass
+
+def pmm_init(bin_edges_1, cdf_1, bin_edges_2, cdf_2):
+    pmm = {}
+    
+    pmm["bin_edges_1"]      = bin_edges_1.copy()
+    pmm["cdf_1"]            = cdf_1.copy()
+    pmm["bin_edges_2"]      = bin_edges_2.copy()
+    pmm["cdf_2"]            = cdf_2.copy()
+    pmm["cdf_interpolator"] = sip.interp1d(bin_edges_1, cdf_1, kind="linear")
+    
+    return pmm
+
+def pmm_compute(pmm, x):
+    mask = np.logical_and(x >= pmm["bin_edges_1"][0], x <= pmm["bin_edges_1"][-1])
+    p = pmm["cdf_interpolator"](x[mask])
+    
+    result = np.ones(len(mask)) * np.nan
+    result[mask] = _invfunc(p, pmm["bin_edges_2"], pmm["cdf_2"])
+    
+    return result
+
+def _invfunc(y, fx, fy):
+  if len(y) == 0:
+      return np.array([])
+  
+  b = np.digitize(y, fy)
+  mask = np.logical_and(b > 0, b < len(fy))
+  c = (y[mask] - fy[b[mask]-1]) / (fy[b[mask]] - fy[b[mask]-1])
+  
+  result = np.ones(len(y)) * np.nan
+  result[mask] = c * fx[b[mask]] + (1.0-c) * fx[b[mask]-1]
+  
+  return result
