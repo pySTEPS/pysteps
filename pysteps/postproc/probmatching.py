929c45bdd779f9b46d97bc01810941ed161072bd
Seppo Pulkkinen <pulkkins@gmail.com>
2018-07-31 18:02:15 +0200

    Add missing docstrings

--- pysteps/postproc/probmatching.py
+++ pysteps/postproc/probmatching.py
@@ -4,6 +4,21 @@ import numpy as np
 from scipy import interpolate as sip
 
 def compute_empirical_cdf(bin_edges, hist):
+    """Compute an empirical cumulative distribution function from the given 
+    histogram.
+    
+    Parameters
+    ----------
+    bin_edges : array_like
+        Coordinates of left edges of the histogram bins.
+    hist : array_like
+        Histogram counts for each bin.
+    
+    Returns
+    -------
+    out : ndarray
+        CDF values corresponding to the bin edges.
+    """
     cdf = []
     xs = 0.0
     
@@ -17,10 +32,9 @@ def compute_empirical_cdf(bin_edges, hist):
     return cdf
 
 def nonparam_match_empirical_cdf(R, R_trg):
-    """Matches the empirical CDF of the initial array with the empirical CDF
-    of a target array. 
-    Initial ranks are conserved, but empirical distribution matches the target one.
-    Zero-pixels in initial array are conserved.
+    """Matches the empirical CDF of the initial array with the empirical CDF 
+    of a target array. Initial ranks are conserved, but empirical distribution 
+    matches the target one. Zero-pixels in initial array are conserved.
     
     Parameters
     ----------
@@ -28,11 +42,11 @@ def nonparam_match_empirical_cdf(R, R_trg):
         The initial array whose CDF is to be changed.
     R_trg : array_like
         The target array whose CDF is to be matched.
-        
+    
     Returns
     -------
-    outputarray : array-like
-        The new array. 
+    out : array_like
+        The new array.
     """
     if R.size != R_trg.size:
         raise ValueError("the input arrays must have the same size")
@@ -81,10 +95,26 @@ def nonparam_match_empirical_cdf(R, R_trg):
     
     return R
 
+# TODO: What is this?
 def nonparam_match_empirical_cdf_masked():
     pass
 
+# TODO: A more detailed explanation of the PMM method + references.
 def pmm_init(bin_edges_1, cdf_1, bin_edges_2, cdf_2):
+    """Initialize a probability matching method (PMM) object from binned 
+    cumulative distribution functions (CDF).
+    
+    Parameters
+    ----------
+    bin_edges_1 : array_like
+        Coordinates of the left bin edges of the source cdf.
+    cdf_1 : array_like
+        Values of the source CDF at the bin edges.
+    bin_edges_2 : array_like
+        Coordinates of the left bin edges of the target cdf.
+    cdf_2 : array_like
+        Values of the target CDF at the bin edges.
+    """
     pmm = {}
     
     pmm["bin_edges_1"]      = bin_edges_1.copy()
@@ -96,6 +126,17 @@ def pmm_init(bin_edges_1, cdf_1, bin_edges_2, cdf_2):
     return pmm
 
 def pmm_compute(pmm, x):
+    """For a given PMM object and x-coordinate, compute the probability matched 
+    value (i.e. the x-coordinate for which the target CDF has the same value as 
+    the source CDF).
+    
+    Parameters
+    ----------
+    pmm : dict
+        A PMM object returned by pmm_init.
+    x : float
+        The coordinate for which to compute the probability matched value.
+    """
     mask = np.logical_and(x >= pmm["bin_edges_1"][0], x <= pmm["bin_edges_1"][-1])
     p = pmm["cdf_interpolator"](x[mask])
     
