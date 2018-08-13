c8edccdda94eae82feb63baaeb282c8a1207f340
Daniele Nerini <daniele.nerini@meteoswiss.ch>
2018-08-13 15:19:26 +0200

    Remove metranet name

--- pysteps/io/interface.py
+++ pysteps/io/interface.py
@@ -47,8 +47,6 @@ def get_method(name, type):
             return importers.import_fmi_pgm
         elif name.lower() == "mch_gif":
             return importers.import_mch_gif
-        elif name.lower() == "mch_metranet":
-            return importers._import_mch_metranet
         elif name.lower() == "odim_hdf5":
             return importers.import_odim_hdf5
         else:
