f7751f60ec8b0b29015ce572d24f2f337b617fa8
Seppo Pulkkinen <pulkkins@gmail.com>
2018-10-04 16:05:39 -0600

    Add datasource configuration file for OPERA rain rate composites

---
+++ config/datasource_opera.py
@@ -0,0 +1,10 @@
+# data source specifications
+root_path  = ""
+path_fmt   = "radar/OPERA/%Y%m%d"
+fn_pattern = "T_PAAH21_C_EUOC_%Y%m%d%H%M%S"
+fn_ext     = "hdf"
+importer   = "odim_hdf5"
+timestep   = 15
+
+# importer arguments
+importer_kwargs = {}
