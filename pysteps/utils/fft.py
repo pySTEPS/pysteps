aa06c9c716abaf030e0c5ef6beb08d17c1ded609
Seppo Pulkkinen <pulkkins@gmail.com>
2018-12-13 18:51:10 -0700

    Initial commit

--- pysteps/utils/fft.py
+++ pysteps/utils/fft.py
@@ -1,2 +1,32 @@
 """Interface module for different FFT methods."""
 
+try:
+    import pyfftw.interfaces.numpy_fft as pyfftw_fft
+    import pyfftw
+    # TODO: Caching and multithreading are currently disabled because they give 
+    # a segfault with dask.
+    pyfftw.interfaces.cache.disable()
+    pyfftw_kwargs = {"threads":1, "planner_effort":"FFTW_ESTIMATE"}
+    pyfftw_imported = True
+except ImportError:
+    pyfftw_imported = False
+try:
+    import scipy.fftpack as scipy_fft
+    scipy_fft_kwargs = {}
+except ImportError:
+    scipy_imported = False
+try:
+    import numpy.fft as numpy_fft
+    numpy_fft_kwargs = {}
+except ImportError:
+    numpy_imported = False
+
+def get_method(name):
+    if name == "numpy":
+        return numpy_fft
+    elif name == "scipy":
+        return scipy_fft
+    elif name == "pyfftw":
+        return pyfftw_fft
+    else:
+        raise ValueError("unknown method %s, the available methods are 'numpy', 'scipy' and 'pyfftw'" % name)
