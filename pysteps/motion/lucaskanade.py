d44fd6fd4b39619a721ef7e320e69f817b907674
ned <daniele.nerini@meteoswiss.ch>
2019-03-06 10:09:14 +0100

    Support for MaskedArray, mask buffering, PEP and docstring improvements8
    Now a quality mask can be defined so that velocities vectors are not computed nearby the range limit of the radars.

--- pysteps/motion/lucaskanade.py
+++ pysteps/motion/lucaskanade.py
@@ -2,7 +2,7 @@
 vectors for areas with no precipitation."""
 
 import numpy as np
-
+from numpy.ma.core import MaskedArray
 from pysteps.exceptions import MissingOptionalDependency
 
 try:
@@ -14,115 +14,145 @@ import scipy.spatial
 import time
 
 def dense_lucaskanade(R, **kwargs):
-    """OpenCV implementation of the Lucas-Kanade method with interpolated motion
-    vectors for areas with no precipitation.
+    """`OpenCV`_ implementation of the local `Lucas-Kanade`_ method with
+    interpolation of the sparse motion vectors to fill the whole grid.
+
+    .. _`OpenCV`: https://opencv.org/
+
+    .. _`Lucas-Kanade`: https://docs.opencv.org/3.4/dc/d6b/\
+        group__video__track.html#ga473e4b886d0bcc6b65831eb88ed93323
 
     Parameters
     ----------
-    R : ndarray_ or MaskedArray, shape (t,m,n)
-        Input images, sequence of 2D arrays, or 3D arrays.
-        The first dimension represents the images time dimension.
+    R : ndarray_ or MaskedArray_
+        Array of shape (T,m,n) containing a sequence of T two-dimensional input
+        images of shape (m,n). 
+        In case of an ndarray_, invalid values (Nans or infs) are masked.
+        The mask in the MaskedArray_ defines a region where velocity vectors are 
+        not computed.
 
     Other Parameters
     ----------------
     max_corners_ST : int
         Maximum number of corners to return. If there are more corners than are
         found, the strongest of them is returned.
-        
+        default : 500
+
     quality_level_ST : float
         Parameter characterizing the minimal accepted quality of image corners.
         See original documentation for more details (https://docs.opencv.org).
-        
+        default : 0.1
+
     min_distance_ST : int
         minimum possible Euclidean distance between the returned corners [px].
-        
+        default : 3
+
     block_size_ST : int
         Size of an average block for computing a derivative covariation matrix
         over each pixel neighborhood.
-        
+        default : 15
+
     winsize_LK : int
         Size of the search window at each pyramid level.
-        Small windows (e.g. 10) lead to unrealistic motion.
-        
+        default : (50, 50)
+
     nr_levels_LK : int
         0-based maximal pyramid level number.
-        Not very sensitive parameter.
-        
+        default : 3
+
     nr_IQR_outlier : int
-        Nr of IQR above median to consider the velocity vector as outlier and 
+        Nr of IQR above median to consider the velocity vector as outlier and
         discard it.
-        
+        default : 3
+
     size_opening : int
         The structuring element size for the filtering of isolated pixels [px].
-        
+        default : 3
+
     decl_grid : int
         Size of the declustering grid [px].
-        
+        default : 20
+
     min_nr_samples : int
-        The minimum number of samples for computing the median within given 
+        The minimum number of samples for computing the median within given
         declustering cell.
-        
+        default : 2
+
     function : string
         The radial basis function, based on the Euclidian norm d, used in the
         interpolation of the sparse vectors.
         Default : inverse
         Available : nearest, inverse, gaussian
-        
+
     k : int or "all"
         The number of nearest neighbors used to speed-up the interpolation.
         If set equal to "all", it employs all the sparse vectors.
         default : 20
-        
+
     epsilon : float
         Adjustable constant for gaussian or inverse functions.
         Default : median distance between sparse vectors.
-        
+
     nchunks : int
         Split the grid points in n chunks to limit the memory usage during the
         interpolation.
         default : 5
-        
+
     extra_vectors : ndarray_
         Additional sparse motion vectors as 2d array (columns: x,y,u,v; rows:
-        nbr. of vectors) to be integrated with the sparse vectors from the 
+        nbr. of vectors) to be integrated with the sparse vectors from the
         Lucas-Kanade local tracking.
         x and y must be in pixel coordinates, with (0,0) being the upper-left
         corner of the field R. u and v must be in pixel units.
-        
+        default : None
+
     verbose : bool
-        if set to True, it prints information about the program
+        If set to True, it prints information about the program
+        default : True
+
+    buffer_mask : int
+        Buffer width in grid points. A border is added to the input mask array
+        to avoid erroneous interpretation of velocities near the maximum range
+        of the radars.
+        default : 0
 
     Returns
     -------
-    out : ndarray_, shape (2,m,n)
-        Three-dimensional array containing the dense x- and y-components of the
-        motion field. Return an empty array when no motion vectors are found.
+    out : ndarray_
+        Three-dimensional array (2,H,W) containing the dense x- and y-components
+        of the motion field.
+        Return an empty array when no motion vectors are found.
+
+    .. _MaskedArray: https://docs.scipy.org/doc/numpy/reference/\
+        maskedarray.baseclass.html#numpy.ma.MaskedArray
+
+    .. _ndarray:\
+    https://docs.scipy.org/doc/numpy/reference/generated/numpy.ndarray.html
 
     """
 
     if len(R.shape) != 3:
         raise ValueError("R has %i dimensions, but a three-dimensional array is expected" % len(R.shape))
+
     if R.shape[0] < 2:
         raise ValueError("R has %i frame, but at least two frames are expected" % R.shape[0])
-    if np.any(~np.isfinite(R)):
-        raise ValueError("All values in R must be finite")
 
     # defaults
-    max_corners_ST      = kwargs.get("max_corners_ST", 500)
-    quality_level_ST    = kwargs.get("quality_level_ST", 0.1)
-    min_distance_ST     = kwargs.get("min_distance_ST", 5)
-    block_size_ST       = kwargs.get("block_size_ST", 15)
-    winsize_LK          = kwargs.get("winsize_LK5", (50, 50))
-    nr_levels_LK        = kwargs.get("nr_levels_LK", 2)
-    nr_IQR_outlier      = kwargs.get("nr_IQR_outlier", 2)
-    size_opening        = kwargs.get("size_opening", 3)
-    decl_grid           = kwargs.get("decl_grid", 20)
-    min_nr_samples      = kwargs.get("min_nr_samples", 2)
-    function            = kwargs.get("function", "inverse")
-    k                   = kwargs.get("k", 20)
-    epsilon             = kwargs.get("epsilon", None)
-    nchunks             = kwargs.get("nchunks", 5)
-    extra_vectors       = kwargs.get("extra_vectors", None)
+    max_corners_ST = kwargs.get("max_corners_ST", 500)
+    quality_level_ST = kwargs.get("quality_level_ST", 0.1)
+    min_distance_ST = kwargs.get("min_distance_ST", 3)
+    block_size_ST = kwargs.get("block_size_ST", 15)
+    winsize_LK = kwargs.get("winsize_LK5", (50, 50))
+    nr_levels_LK = kwargs.get("nr_levels_LK", 3)
+    nr_IQR_outlier = kwargs.get("nr_IQR_outlier", 3)
+    size_opening = kwargs.get("size_opening", 3)
+    decl_grid = kwargs.get("decl_grid", 20)
+    min_nr_samples = kwargs.get("min_nr_samples", 2)
+    function = kwargs.get("function", "inverse")
+    k = kwargs.get("k", 20)
+    epsilon = kwargs.get("epsilon", None)
+    nchunks = kwargs.get("nchunks", 5)
+    extra_vectors = kwargs.get("extra_vectors", None)
     if extra_vectors is not None:
         if len(extra_vectors.shape) != 2:
             raise ValueError("extra_vectors has %i dimensions, but 2 dimensions are expected"
@@ -130,39 +160,59 @@ def dense_lucaskanade(R, **kwargs):
         if extra_vectors.shape[1] != 4:
             raise ValueError("extra_vectors has %i columns, but 4 columns are expected"
                                % extra_vectors.shape[1])
-    verbose             = kwargs.get("verbose", True)
+    verbose = kwargs.get("verbose", True)
+    buffer_mask = kwargs.get("buffer_mask", 0)
+
     if verbose:
         print("Computing the motion field with the Lucas-Kanade method.")
         t0 = time.time()
 
+    # Get mask
+    if isinstance(R, MaskedArray):
+        mask = np.ma.getmaskarray(R).copy()
+    else:
+        R = np.ma.masked_invalid(R)
+        mask = np.ma.getmaskarray(R).copy()
+    R[mask] = np.nanmin(R)  # Remove any Nan from the raw data
+
     nr_fields = R.shape[0]
     domain_size = (R.shape[1], R.shape[2])
-    y0Stack=[]
-    x0Stack=[]
-    uStack=[]
-    vStack=[]
-    for n in range(nr_fields-1):
+    y0Stack = []
+    x0Stack = []
+    uStack = []
+    vStack = []
+    for n in range(nr_fields - 1):
 
         # extract consecutive images
-        prvs = R[n,:,:].copy()
-        next = R[n+1,:,:].copy()
+        prvs = R[n, :, :].copy()
+        next = R[n + 1, :, :].copy()
 
         # scale between 0 and 255
         prvs = (prvs - prvs.min())/(prvs.max() - prvs.min())*255
         next = (next - next.min())/(next.max() - next.min())*255
 
         # convert to 8-bit
-        prvs = np.ndarray.astype(prvs,"uint8")
-        next = np.ndarray.astype(next,"uint8")
+        prvs = np.ndarray.astype(prvs, "uint8")
+        next = np.ndarray.astype(next, "uint8")
+        mask_ = np.ndarray.astype(mask[n, :, :], "uint8")
+
+        # buffer the quality mask to ensure that no vectors are computed nearby
+        # the edges of the radar mask
+        if buffer_mask > 0:
+            mask_ = cv2.morphologyEx(mask_, cv2.MORPH_DILATE,
+                                     np.ones((int(buffer_mask), int(buffer_mask)), 
+                                     np.uint8))
 
         # remove small noise with a morphological operator (opening)
-        prvs = _clean_image(prvs, n=size_opening)
-        next = _clean_image(next, n=size_opening)
+        if size_opening > 0:
+            prvs = _clean_image(prvs, n=size_opening)
+            next = _clean_image(next, n=size_opening)
 
         # Shi-Tomasi good features to track
         # TODO: implement different feature detection algorithms (e.g. Harris)
+        mask_ = (-1*mask_ + 1).astype('uint8')
         p0 = _ShiTomasi_features_to_track(prvs, max_corners_ST, quality_level_ST,
-                                          min_distance_ST, block_size_ST)
+                                          min_distance_ST, block_size_ST, mask_)
 
         # get sparse u, v vectors with Lucas-Kanade tracking
         x0, y0, u, v = _LucasKanade_features_tracking(prvs, next, p0, winsize_LK,
@@ -170,15 +220,15 @@ def dense_lucaskanade(R, **kwargs):
 
         # exclude outlier vectors
         vel = np.sqrt(u**2 + v**2) # [px/timesteps]
-        q1, q2 = np.percentile(vel, [25,75])
+        q1, q2 = np.percentile(vel, [25, 75])
         min_speed_thr = np.max((0, q1 - nr_IQR_outlier*(q2 - q1)))
         max_speed_thr = q2 + nr_IQR_outlier*(q2 - q1)
         keep = np.logical_and(vel < max_speed_thr, vel > min_speed_thr)
 
-        u = u[keep][:,None]
-        v = v[keep][:,None]
-        y0 = y0[keep][:,None]
-        x0 = x0[keep][:,None]
+        u = u[keep][:, None]
+        v = v[keep][:, None]
+        y0 = y0[keep][:, None]
+        x0 = x0[keep][:, None]
 
         # stack vectors within time window
         y0Stack.append(y0)
@@ -210,29 +260,38 @@ def dense_lucaskanade(R, **kwargs):
         UV = np.empty((2, domain_size[0], domain_size[1]))
 
     if verbose:
-        print("--- %s seconds ---" % (time.time() - t0))
+        print("--- %.2f seconds ---" % (time.time() - t0))
 
     return UV
 
 def _ShiTomasi_features_to_track(R, max_corners_ST, quality_level_ST,
-                                 min_distance_ST, block_size_ST):
+                                 min_distance_ST, block_size_ST, mask):
     """Call the Shi-Tomasi corner detection algorithm.
 
     Parameters
     ----------
     R : array-like
-        Array of shape (m,n) containing the input precipitation field passed as 8-bit image.
+        Array of shape (m,n) containing the input precipitation field passed as
+        8-bit image.
+
     max_corners_ST : int
         Maximum number of corners to return. If there are more corners than are
         found, the strongest of them is returned.
+
     quality_level_ST : float
         Parameter characterizing the minimal accepted quality of image corners.
         See original documentation for more details (https://docs.opencv.org).
+
     min_distance_ST : int
         Minimum possible Euclidean distance between the returned corners [px].
+
     block_size_ST : int
         Size of an average block for computing a derivative covariation matrix
         over each pixel neighborhood.
+
+    mask : ndarray_
+        Array of shape (m,n). It specifies the region in which the corners are
+        detected.
 
     Returns
     -------
@@ -255,7 +314,7 @@ def _ShiTomasi_features_to_track(R, max_corners_ST, quality_level_ST,
                             minDistance=min_distance_ST, blockSize=block_size_ST)
 
     # detect corners
-    p0 = cv2.goodFeaturesToTrack(R, mask=None, **ShiTomasi_params)
+    p0 = cv2.goodFeaturesToTrack(R, mask=mask, **ShiTomasi_params)
 
     if p0 is None:
         raise ValueError("Shi-Tomasi found no good feature to be tracked.")
@@ -299,23 +358,23 @@ def _LucasKanade_features_tracking(prvs, next, p0, winsize_LK, nr_levels_LK):
             "optical flow method but it is not installed")
 
     # LK parameters
-    lk_params = dict( winSize=winsize_LK, maxLevel=nr_levels_LK,
+    lk_params = dict(winSize=winsize_LK, maxLevel=nr_levels_LK,
                      criteria=(cv2.TERM_CRITERIA_EPS|cv2.TERM_CRITERIA_COUNT, 10, 0))
 
     # Lucas-Kande
     p1, st, err = cv2.calcOpticalFlowPyrLK(prvs, next, p0, None, **lk_params)
 
     # keep only features that have been found
-    st = st[:,0]==1
-    p1 = p1[st,:,:]
-    p0 = p0[st,:,:]
-    err = err[st,:]
+    st = st[:, 0] == 1
+    p1 = p1[st, :, :]
+    p0 = p0[st, :, :]
+    err = err[st, :]
 
     # extract vectors
-    x0 = p0[:,:,0]
-    y0 = p0[:,:,1]
-    u = np.array((p1-p0)[:,:,0])
-    v = np.array((p1-p0)[:,:,1])
+    x0 = p0[:, :, 0]
+    y0 = p0[:, :, 1]
+    u = np.array((p1 - p0)[:, :, 0])
+    v = np.array((p1 - p0)[:, :, 1])
 
     return x0, y0, u, v
 
@@ -341,12 +400,12 @@ def _clean_image(R, n=3, thr=0):
         raise MissingOptionalDependency(
             "opencv package is required for the Lucas-Kanade method "
             "optical flow method but it is not installed")
-        
+
     # convert to binary image (rain/no rain)
     field_bin = np.ndarray.astype(R > thr,"uint8")
 
     # build a structuring element of size (nx)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (n,n))
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (n, n))
 
     # apply morphological opening (i.e. erosion then dilation)
     field_bin_out = cv2.morphologyEx(field_bin, cv2.MORPH_OPEN, kernel)
@@ -406,16 +465,16 @@ def _declustering(x, y, u, v, decl_grid, min_nr_samples):
     yT = np.floor(yT)
 
     # keep only unique combinations of coordinates
-    xy    = np.concatenate((xT,yT), axis=1)
-    xyb   = np.ascontiguousarray(xy).view(np.dtype((np.void, xy.dtype.itemsize*xy.shape[1])))
+    xy = np.concatenate((xT,yT), axis=1)
+    xyb = np.ascontiguousarray(xy).view(np.dtype((np.void, xy.dtype.itemsize*xy.shape[1])))
     _,idx = np.unique(xyb, return_index=True)
-    uxy   = xy[idx]
+    uxy = xy[idx]
 
     # now loop through these unique values and average vectors which belong to
     # the same declustering grid cell
-    xN=[]; yN=[]; uN=[]; vN=[]
+    xN = []; yN = []; uN = []; vN = []
     for i in range(uxy.shape[0]):
-        idx = np.logical_and(xT==uxy[i,0], yT==uxy[i,1])
+        idx = np.logical_and(xT == uxy[i, 0], yT == uxy[i, 1])
         npoints = np.sum(idx)
         if npoints >= min_nr_samples:
             xN.append(np.median(x[idx]))
