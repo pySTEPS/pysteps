a1376e3a74f61e3944395f22011e70942c6981a5
Andres Perez Hortal <andresperezcba@gmail.com>
2019-03-19 11:20:15 -0400

    Fix path.insert bug

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,7 +20,8 @@ import sys
 #
 import os
 
-sys.path.insert(0, os.path.abspath('.'), os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(1, os.path.abspath('../../'))
 
 print("-----------------------------------------")
 print("-----------------------------------------")
