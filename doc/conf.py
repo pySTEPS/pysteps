e3ab5c59f5f3f87a732d09cd0a6ffcb287099833
Andres Perez Hortal <andresperezcba@gmail.com>
2019-03-19 09:42:37 -0400

    Build in-place

--- doc/conf.py
+++ doc/conf.py
@@ -20,10 +20,7 @@ import sys
 #
 import os
 
-try:
-    import pysteps
-except ImportError:
-    sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- General configuration ------------------------------------------------
 
