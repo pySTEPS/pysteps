fb37a42559c03f5ab7f941728d59a645396efc51
Carlos Velasco <carlos.velasco@bom.gov.au>
2019-03-19 13:24:32 +0000

    Add test score as string

--- pysteps/tests/test_detcontscores.py
+++ pysteps/tests/test_detcontscores.py
@@ -17,6 +17,8 @@ test_data = [
     ([0.], [0.], None, []),
     # test unknown score
     ([1., 3.], [2., 5.], ('lolo'), []),
+    # Mean Absolute Error (Additive) as string
+    (fct_data, obs_data, 'MAE_ADD', [2.8]),
     # Mean Absolute Error (Additive)
     (fct_data, obs_data, ('MAE_ADD'), [2.8]),
     # Mean Absolute Error (Multiplicative)
