42a3c4d7c7f2e7e4fdee4b1827b7c83a0aa621aa
Carlos Velasco <carlos.velasco@bom.gov.au>
2019-03-25 14:26:24 +0000

    Add tests for excprob

--- pysteps/tests/test_ensemblestats.py
+++ pysteps/tests/test_ensemblestats.py
@@ -3,6 +3,7 @@
 import pytest
 import numpy as np
 from pysteps.postprocessing.ensemblestats import mean
+from pysteps.postprocessing.ensemblestats import excprob
 from numpy.testing import assert_array_almost_equal
 
 # CREATE DATASETS TO TEST
@@ -16,7 +17,7 @@ a1[:] = np.nan
 a2 = a.copy()
 a2[0, :] = np.nan
 
-
+#  test data
 test_data = [
     (a, False, None, a),
     (b, False, None, a),
@@ -35,7 +36,7 @@ def test_ensemblestats_mean(X, ignore_nan, X_thr, expected):
     assert_array_almost_equal(mean(X, ignore_nan, X_thr), expected)
 
 
-# test exceptions
+#  test exceptions
 test_exceptions = [(0), (None), (a[0, :]),
                    (np.tile(a, (4, 1, 1, 1))),
                    ]
@@ -46,3 +47,32 @@ def test_exceptions_mean(X):
     with pytest.raises(Exception):
         mean(X)
 
+
+#  test data
+b2 = b.copy()
+b2[2, 2, 2] = np.nan
+
+test_data = [
+    (b, 2.0, False, np.array([[0., 0., 1.], [1., 1., 1.], [1., 1., 1.]])),
+    (b2, 2.0, False, np.array([[0., 0., 1.], [1., 1., 1.], [1., 1., np.nan]])),
+    (b2, 2.0, True, np.array([[0., 0., 1.], [1., 1., 1.], [1., 1., 1.]])),
+    ]
+
+
+@pytest.mark.parametrize("X, X_thr, ignore_nan, expected", test_data)
+def test_ensemblestats_excprob(X, X_thr, ignore_nan, expected):
+    """Test ensemblestats excprob."""
+    assert_array_almost_equal(excprob(X, X_thr, ignore_nan), expected)
+
+
+#  test exceptions
+test_exceptions = [(0), (None),
+                   (a[0, :]),
+                   (a),
+                   ]
+
+
+@pytest.mark.parametrize("X", test_exceptions)
+def test_exceptions_excprob(X):
+    with pytest.raises(Exception):
+        excprob(X, 2.0)
