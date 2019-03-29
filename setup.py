0c314c894c1ee9a0d52f19e701394aeab8e8d193
ned <daniele.nerini@meteoswiss.ch>
2019-03-29 09:00:53 +0100

    Update version, add author, url and long_description_content_type

--- setup.py
+++ setup.py
@@ -14,12 +14,12 @@ except ImportError:
 
 extra_link_args = ['-fopenmp']
 
-if sys.platform.startswith('darwin'):
-    extra_link_args.append('-Wl,-rpath,/usr/local/opt/gcc/lib/gcc/8/')
+if sys.platform.startswith("darwin"):
+    extra_link_args.append("-Wl,-rpath,/usr/local/opt/gcc/lib/gcc/8/")
 
-_vet_extension_arguments = dict(extra_compile_args=['-fopenmp'],
+_vet_extension_arguments = dict(extra_compile_args=["-fopenmp"],
                                 include_dirs=[numpy.get_include()],
-                                language='c',
+                                language="c",
                                 extra_link_args=extra_link_args,
                                 )
 
@@ -27,36 +27,40 @@ try:
     from Cython.Build.Dependencies import cythonize
 
     _vet_lib_extension = Extension(str("pysteps.motion._vet"),
-                                   sources=[str('pysteps/motion/_vet.pyx')],
+                                   sources=[str("pysteps/motion/_vet.pyx")],
                                    **_vet_extension_arguments)
 
     external_modules = cythonize([_vet_lib_extension])
 
 except ImportError:
     _vet_lib_extension = Extension(str(str("pysteps.motion._vet")),
-                                   sources=[str('pysteps/motion/_vet.c')],
+                                   sources=[str("pysteps/motion/_vet.c")],
                                    **_vet_extension_arguments)
     external_modules = [_vet_lib_extension]
 
-requirements = ['numpy',
-                'attrdict', 'jsmin', 'scipy', 'matplotlib',
-                'jsonschema']
+requirements = ["numpy",
+                "attrdict", "jsmin", "scipy", "matplotlib",
+                "jsonschema"]
 
 setup(
-    name='pysteps',
-    version='0.2',
+    name="pysteps",
+    version="1.0.0",
+    author="PySteps developers"
     packages=find_packages(),
-    license='LICENSE',
+    license="LICENSE",
     include_package_data=True,
-    description='Python framework for short-term ensemble prediction systems',
-    long_description=open('README.rst').read(),
+    description="Python framework for short-term ensemble prediction systems",
+    long_description=open("README.rst").read(),
+    long_description_content_type="text/x-rst",
+    url="https://pysteps.github.io/",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Atmospheric Science',
-        'License :: OSI Approved :: The 3-Clause BSD License (BSD-3-Clause)',
-        'Programming Language :: Python :: 3'],
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+        "License :: OSI Approved :: The 3-Clause BSD License (BSD-3-Clause)",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"],
     ext_modules=external_modules,
     setup_requires=requirements,
     install_requires=requirements
