ce352be432c8685fd21bf47deda26e808ad151cd
ned <daniele.nerini@meteoswiss.ch>
2019-04-03 16:41:54 +0200

    Format with black

--- pysteps/io/utils.py
+++ pysteps/io/utils.py
@@ -13,12 +13,13 @@ Miscellaneous utility functions for the io module.
 
 import os
 
+
 def get_pysteps_data_rootpath():
-    stp_data_path = os.environ.get('PYSTEPS_DATA', None)
+    stp_data_path = os.environ.get("PYSTEPS_DATA", None)
     if stp_data_path is None:
         raise EnvironmentError("'PYSTEPS_DATA' environment variable not set")
     if not os.path.isdir(stp_data_path):
-        raise EnvironmentError("'PYSTEPS_DATA' path '{0}' "
-                               "does not exist".format(stp_data_path))
+        raise EnvironmentError(
+            "'PYSTEPS_DATA' path '{0}' " "does not exist".format(stp_data_path)
+        )
     return stp_data_path
-
