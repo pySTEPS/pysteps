ea371b1c783f11a0f36c901bf40bfe298901ef31
ned <daniele.nerini@meteoswiss.ch>
2019-04-03 16:24:15 +0200

    Fix alignment

--- pysteps/io/utils.py
+++ pysteps/io/utils.py
@@ -9,7 +9,7 @@ Miscellaneous utility functions for the io module.
 
     get_pysteps_data_path
     get_pysteps_data_file
-    
+
 """
 
 import os
@@ -21,11 +21,11 @@ def get_pysteps_data_path():
     if not os.path.isdir(stp_data_path):
         raise EnvironmentError("'PYSTEPS_DATA' path '{0}' "
                                "does not exist".format(stp_data_path))
-return stp_data_path
+    return stp_data_path
 
 def get_pysteps_data_file(relfile):
     data_file = os.path.join(get_pysteps_data_path(), relfile)
     if not os.path.exists(data_file):
         raise EnvironmentError("PYSTEPS_DATA file '{0}' "
                                "does not exist".format(data_file))
-return data_file
+    return data_file
