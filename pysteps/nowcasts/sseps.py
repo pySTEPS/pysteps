Fix normalization of mask

pysteps/nowcasts/sseps.py
@@ -659,7 +659,8 @@ def _compute_incremental_mask(Rbin, kr, r):
     for n in range(r):
         Rd = scipy.ndimage.morphology.binary_dilation(Rd, kr1)
         mask += Rd
-    return mask
+    # normalize between 0 and 1
+    return mask / mask.max()
 
 
 def _recompose_cascade(R, mu, sigma):

pysteps/nowcasts/steps.py
@@ -676,7 +676,7 @@ def _compute_incremental_mask(Rbin, kr, r):
         Rd = scipy.ndimage.morphology.binary_dilation(Rd, kr1)
         mask += Rd
     # normalize between 0 and 1
-    return mask / (r + 1)
+    return mask / mask.max()
 
 
 def _compute_sprog_mask(R, war):