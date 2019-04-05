e4b98ea7cdc967564cc6a34b4e1b127dd6df9115
ned <daniele.nerini@meteoswiss.ch>
2019-04-05 17:58:02 +0200

    Remove scripts

--- scripts/read_the_docs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export PYSTEPS_BUILD_DIR="$( pwd )"
-git clone https://github.com/pySTEPS/pysteps-data.git $PYSTEPS_BUILD_DIR/pysteps-data
-python scripts/read_the_docs.py
-export PYSTEPSRC=$PYSTEPS_BUILD_DIR/pystepsrc.rtd
