c126ac76bbf7e50e6708ec8fb0b8d22ff6bf191f
ned <daniele.nerini@meteoswiss.ch>
2019-04-05 11:38:22 +0200

    Join paths

--- scripts/travis.py
+++ scripts/travis.py
@@ -4,16 +4,16 @@ import os
 import json
 from jsmin import jsmin
 
-with open("pysteps/pystepsrc", 'r') as f:
+with open(os.path.join("pysteps", "pystepsrc"), "r") as f:
     rcparams = json.loads(jsmin(f.read()))
 
-for key, value in rcparams['data_sources'].items():
-    original_path = value['root_path']
+for key, value in rcparams["data_sources"].items():
+    original_path = value["root_path"]
 
-    new_path = os.path.join("pysteps-data", value['root_path'])
+    new_path = os.path.join("pysteps-data", value["root_path"])
     new_path = os.path.abspath(new_path)
 
-    value['root_path'] = new_path
+    value["root_path"] = new_path
 
-with open("pystepsrc.travis", 'w') as f:
+with open("pystepsrc.travis", "w") as f:
     json.dump(rcparams, f, indent=4)
