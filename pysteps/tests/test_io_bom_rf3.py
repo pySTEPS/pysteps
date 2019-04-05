0a41c8a1fe01168202e930baf5352746e911b98d
ned <daniele.nerini@meteoswiss.ch>
2019-04-05 11:06:45 +0200

    Fix relative path to data

--- pysteps/tests/test_io_bom_rf3.py
+++ pysteps/tests/test_io_bom_rf3.py
@@ -8,7 +8,7 @@ import pysteps
 def test_io_import_bom_rf3_shape():
     """Test the importer Bom RF3."""
     root_path = pysteps.rcparams.data_sources["bom"]["root_path"]
-    rel_path = os.path.join("radar", "bom", "prcp-cscn", "2", "2018", "06", "16")
+    rel_path = os.path.join("prcp-cscn", "2", "2018", "06", "16")
     filename = os.path.join(root_path, rel_path, "2_20180616_100000.prcp-cscn.nc")
     R, _, _ = pysteps.io.import_bom_rf3(filename)
     assert R.shape == (512, 512)
@@ -35,9 +35,9 @@ test_metadata_bom = [
 @pytest.mark.parametrize("variable,expected", test_metadata_bom)
 def test_io_import_bom_rf3_metadata(variable, expected):
     """Test the importer Bom RF3."""
-    rootpath = pysteps.io.get_pysteps_data_rootpath()
-    relpath = os.path.join("radar", "bom", "prcp-cscn", "2", "2018", "06", "16")
-    filename = os.path.join(rootpath, relpath, "2_20180616_100000.prcp-cscn.nc")
+    root_path = pysteps.rcparams.data_sources["bom"]["root_path"]
+    rel_path = os.path.join("prcp-cscn", "2", "2018", "06", "16")
+    filename = os.path.join(root_path, rel_path, "2_20180616_100000.prcp-cscn.nc")
     _, _, metadata = pysteps.io.import_bom_rf3(filename)
     assert metadata[variable] == expected
 
@@ -61,8 +61,8 @@ test_geodata_bom = [
 @pytest.mark.parametrize("variable,expected", test_geodata_bom)
 def test_io_import_bom_rf3_geodata(variable, expected):
     """Test the importer Bom RF3."""
-    rootpath = pysteps.io.get_pysteps_data_rootpath()
-    relpath = os.path.join("radar", "bom", "prcp-cscn", "2", "2018", "06", "16")
-    filename = os.path.join(rootpath, relpath, "2_20180616_100000.prcp-cscn.nc")
+    root_path = pysteps.rcparams.data_sources["bom"]["root_path"]
+    rel_path = os.path.join("prcp-cscn", "2", "2018", "06", "16")
+    filename = os.path.join(root_path, rel_path, "2_20180616_100000.prcp-cscn.nc")
     geodata = pysteps.io.importers._import_bom_rf3_geodata(filename)
     assert geodata[variable] == expected
