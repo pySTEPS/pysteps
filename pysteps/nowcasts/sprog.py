Fix incorrect normalization

pysteps/nowcasts/sprog.py
@@ -224,12 +224,17 @@ def forecast(R, V, n_timesteps, n_cascade_levels=6, R_thr=None,
 
     # normalize the cascades and rearrange them into a four-dimensional array
     # of shape (n_cascade_levels,ar_order+1,m,n) for the autoregressive model
-    R_c, mu, sigma = nowcast_utils.rearrange_cascades(R_d, n_cascade_levels)
+    donorm = True if domain == "spatial" else False
+    R_c, mu, sigma = nowcast_utils.rearrange_cascades(R_d, n_cascade_levels,
+                                                      donorm=donorm)
 
     # compute lag-l temporal autocorrelation coefficients for each cascade level
     GAMMA = np.empty((n_cascade_levels, ar_order))
     for i in range(n_cascade_levels):
         R_c_ = np.stack([R_c[i][j, :, :] for j in range(ar_order + 1)])
+        # TODO: Don't use this when domain="spectral". The correlation coefficients
+        # need to be computed in the spectral domain because the inverse FFT
+        # has not been applied to the cascade levels.
         GAMMA[i, :] = correlation.temporal_autocorrelation(R_c_, MASK=MASK_thr)
 
     nowcast_utils.print_corrcoefs(GAMMA)
@@ -261,6 +266,9 @@ def forecast(R, V, n_timesteps, n_cascade_levels=6, R_thr=None,
 
         for i in range(n_cascade_levels):
             fb_mask = filter["weights_2d"][i, :, :] > 1e-3
+            # TESTING
+            #fb_mask = np.ones(fb_mask.shape, dtype=bool)
+            #
             filter["weights_masked"].append(filter["weights_2d"][i, fb_mask])
             filter["masks"].append(fb_mask)
             R_c__ = []
@@ -305,8 +313,8 @@ def forecast(R, V, n_timesteps, n_cascade_levels=6, R_thr=None,
             R_c_last = np.stack(R_c_last)
             R_c_ = nowcast_utils.recompose_cascade_spatial(R_c_last, mu, sigma)
         else:
-            R_c_ = nowcast_utils.recompose_cascade_spectral(R_c_last, mu, sigma,
-                                                            filter, fft)
+            R_c_ = nowcast_utils.recompose_cascade_spectral(R_c_last, R.shape,
+                                                            mu, sigma, filter, fft)
 
         MASK = _compute_sprog_mask(R_c_, war)
         R_c_[~MASK] = R_min

pysteps/nowcasts/utils.py
@@ -149,7 +149,7 @@ def recompose_cascade_spatial(R, mu, sigma):
     return R_rc
 
 # TODO: Update the docstring.
-def recompose_cascade_spectral(R, mu, sigma, filter, fft_method):
+def recompose_cascade_spectral(R, R_shape, mu, sigma, filter, fft):
     """Recompose a cascade by inverting the normalization and summing the
     cascade levels.
     
@@ -158,9 +158,19 @@ def recompose_cascade_spectral(R, mu, sigma, filter, fft_method):
     R : array_like
         ...
     """
+    # TESTING
+#    R_rc = np.zeros(R_shape)
+#    for i in range(len(mu)):
+#        R_i = np.zeros((1226, 381), dtype=complex)
+#        R_i[filter["masks"][i]] = R[i]
+#        R_rc += fft.irfft2(R_i) * sigma[i] + mu[i]
+#    
+#    return R_rc
+    #
+    
     R_rc = np.zeros(filter["masks"][0].shape, dtype=complex)
 
     for i in range(len(mu)):
-        R_rc[filter["masks"][i]] += filter["weights_masked"][i] * R[i] * sigma[i] + mu[i]
+        R_rc[filter["masks"][i]] += R[i]
 
-    return fft_method.irfft2(R_rc)
+    return fft.irfft2(R_rc)