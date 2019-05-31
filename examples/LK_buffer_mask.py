432fe98adf74dbfa6265f9dee21ced0ed76abe6f
ned <daniele.nerini@meteoswiss.ch>
2019-05-31 18:12:27 +0200

    Update example

--- examples/LK_buffer_mask.py
+++ examples/LK_buffer_mask.py
@@ -1,22 +1,16 @@
 """
-Lucas-Kanade
-============
+Handling of no-data in Lucas-Kanade
+===================================
 
-This example shows how the Lucas-Kande algorithm can be tuned to avoid the 
-erroneous interpretation of velocities near the maximum range of the radars.
+Areas of missing data in radar images are typically caused by visibility limits
+such as beam blockage and the radar coverage itself. These artifacts can mislead 
+the echo tracking algorithms. For instance, precipitation leaving the domain
+might be erroneously detected as nearly stationary velocities.
 
-The Lucas-Kanade optical flow method identifies features on the input precipitation
-frame and then tries to track them in the successive frame. 
-
-With radar images, the limited extent of the domain means that often
-precipitation leaves or enters the image. 
-The tracking algorithm is particularly challenged by precipitation leaving 
-the domain, which may result in nearly stationary velocities.
-
-In order to address the boundary effects, the buffer_mask argument in the 
-Lucas-Kanade method allows to exclude all vectors detected nearby nodata areas, 
-meaning all those regions affected by blockage, limits of the radar coverage, 
-or other radar artifacts that may mislead the tracking.
+This example shows how the Lucas-Kanade algorithm can be tuned to avoid the 
+erroneous interpretation of velocities near the maximum range of the radars by 
+buffering the no-data mask in the radar image in order to exclude all vectors 
+detected nearby nodata areas.
 """
 
 from datetime import datetime
@@ -72,39 +66,55 @@ del quality  # Not used
 # Convert to mm/h
 R, metadata = conversion.to_rainrate(R, metadata)
 
-# Keep the reference frame in mm/h
+# Keep the reference frame in mm/h and its mask (for plotting purposes)
 ref_mm = R[0, :, :].copy()
-
-# Extract radar mask
 mask = np.ones(ref_mm.shape)
 mask[~np.isnan(ref_mm)] = np.nan
 
 # Log-transform the data [dBR]
 R, metadata = transformation.dB_transform(R, metadata, threshold=0.1, zerovalue=-15.0)
 
-# Keep the reference frame in dBR
+# Keep the reference frame in dBR (for plotting purposes)
 ref_dbr = R[0].copy()
 ref_dbr[ref_dbr < -10] = np.nan
 
-################################################################################
-# sparse Lucas-Kanade
+# Plot the reference field
+plot_precip_field(ref_mm, title="Reference field")
+circle = plt.Circle((620, 400), 100, color="b", clip_on=False, fill=False)
+plt.gca().add_artist(circle)
+plt.show()
+
+###############################################################################
+# Notice the "half-in, half-out" precipitation area within the blue circle.
+# As we are going to show next, the tracking algorithm can erroneously interpret
+# precipitation leaving the domain as stationary motion.
+#
+# Also note that the radar image includes NaNs in areas of missing data.
+# These are used by the optical flow algorithm to define the radar mask.
+#
+# Sparse Lucas-Kanade
 # -------------------
 #
-# By setting the dense=False, the LK algorithm returns the sparse vectors only, 
-# meaning that the interpolation is not performed. This allows us to better 
-# identify the presence of nearly stationary motion in ares where precipitation 
-# is leaving the domain (see blue circle in the figure below).
+# By setting the dense=False, the LK algorithm returns the motion vectors detected
+# by the Lucas-Kanade scheme without interpolating them on the grid. This allows
+# us to better identify the presence of wrongly detected stationary motion in
+# ares where precipitation is leaving the domain (look for the red dots within
+# the blue circle in the left panel of the figure below).
 
-
+# get Lucas-Kanade optical flow method
 LK_optflow = motion.get_method("LK")
 
-# no buffer (default settings)
+# Mask invalid values
+R = np.ma.masked_invalid(R)
+R.data[R.mask] = np.nan
+
+# Use default settings (i.e., no buffering of the radar mask)
 x, y, u, v = LK_optflow(R, dense=False, buffer_mask=0, quality_level_ST=0.1)
 plt.subplot(121)
 plt.imshow(ref_dbr, cmap=plt.get_cmap("Greys"))
-plt.imshow(mask, cmap=colors.ListedColormap(['black']), alpha=0.5)
-plt.quiver(x, y, u, v, color="red", angles="xy", scale_units="xy", scale=.2)
-circle = plt.Circle((620, 245), 100, color='b', clip_on=False, fill=False)
+plt.imshow(mask, cmap=colors.ListedColormap(["black"]), alpha=0.5)
+plt.quiver(x, y, u, v, color="red", angles="xy", scale_units="xy", scale=0.2)
+circle = plt.Circle((620, 245), 100, color="b", clip_on=False, fill=False)
 plt.gca().add_artist(circle)
 plt.title("buffer_mask = 0")
 
@@ -112,9 +122,9 @@ plt.title("buffer_mask = 0")
 x, y, u, v = LK_optflow(R, dense=False, buffer_mask=20, quality_level_ST=0.2)
 plt.subplot(122)
 plt.imshow(ref_dbr, cmap=plt.get_cmap("Greys"))
-plt.imshow(mask, cmap=colors.ListedColormap(['black']), alpha=0.5)
-plt.quiver(x, y, u, v, color="red", angles="xy", scale_units="xy", scale=.2)
-circle = plt.Circle((620, 245), 100, color='b', clip_on=False, fill=False)
+plt.imshow(mask, cmap=colors.ListedColormap(["black"]), alpha=0.5)
+plt.quiver(x, y, u, v, color="red", angles="xy", scale_units="xy", scale=0.2)
+circle = plt.Circle((620, 245), 100, color="b", clip_on=False, fill=False)
 plt.gca().add_artist(circle)
 plt.title("buffer_mask = 20")
 
@@ -125,34 +135,32 @@ plt.show()
 # Dense Lucas-Kanade
 # ------------------
 #
-# The above displacement vectors produced by the Lucas-Kanade method are 
+# The above displacement vectors produced by the Lucas-Kanade method are
 # interpolated to produce a full field of motion (i.e., dense=True).
 # By comparing the x- and y-components of the motion field, we can easily notice
-# negative bias that is introduced by the the erroneous interpretation of 
+# the negative bias that is introduced by the the erroneous interpretation of
 # velocities near the maximum range of the radars.
 
 UV1 = LK_optflow(R, dense=True, buffer_mask=0, quality_level_ST=0.1)
 UV2 = LK_optflow(R, dense=True, buffer_mask=20, quality_level_ST=0.2)
 
-plt.subplot(121)
-plt.imshow((UV1[0] - UV2[0])/UV2[0], cmap=cm.RdBu_r, vmin=-.2, vmax=.2)
-plt.colorbar(fraction=0.04, pad=0.04)
-plt.title(r"(u - u$_{buffer}$)/u$_{buffer}$")
+V1 = np.sqrt(UV1[0] ** 2 + UV1[1] ** 2)
+V2 = np.sqrt(UV2[0] ** 2 + UV2[1] ** 2)
 
-plt.subplot(122)
-plt.imshow((UV1[1] - UV2[1])/UV2[1], cmap=cm.RdBu_r, vmin=-.2, vmax=.2)
+plt.imshow((V1 - V2) / V2, cmap=cm.RdBu_r, vmin=-0.1, vmax=0.1)
 plt.colorbar(fraction=0.04, pad=0.04)
-plt.title(r"(v - v$_{buffer}$)/v$_{buffer}$")
-
-plt.tight_layout()
+plt.title("Relative difference in motion speed")
 plt.show()
 
 ################################################################################
+# Notice that the original motion field is significantly slower (more than 10%
+# slower) because of the presence of wrong stationary motion vectors at the edges.
+#
 # Forecast skill
 # --------------
 #
-# We are going to evaluate the benefit of buffering the radar mask by computing 
-# the forecast error as the correlation coefficient (Spearman).
+# We are now going to evaluate the benefit of buffering the radar mask by computing
+# the forecast skill in terms of the Spearman correlation coefficient.
 
 extrapolate = nowcasts.get_method("extrapolation")
 R[~np.isfinite(R)] = metadata["zerovalue"]
@@ -172,7 +180,7 @@ fns = io.archive.find_by_date(
 R_o, _, metadata_o = io.read_timeseries(fns, importer, **importer_kwargs)
 R_o, metadata_o = conversion.to_rainrate(R_o, metadata_o)
 
-# Compute fractions skill score (FSS) for all lead times, a set of scales and 1 mm/h
+# Compute Spearman correlation
 skill = verification.get_method("corr_s")
 score_1 = []
 score_2 = []
@@ -180,7 +188,7 @@ for i in range(12):
     score_1.append(skill(R_f1[i, :, :], R_o[i + 1, :, :])["corr_s"])
     score_2.append(skill(R_f2[i, :, :], R_o[i + 1, :, :])["corr_s"])
 
-x = (np.arange(12) + 1) * 5 # [min]
+x = (np.arange(12) + 1) * 5  # [min]
 plt.plot(x, score_1, label="no mask buffer")
 plt.plot(x, score_2, label="with mask buffer")
 plt.legend()
@@ -191,4 +199,4 @@ plt.title("Spearman correlation")
 plt.tight_layout()
 plt.show()
 
-# sphinx_gallery_thumbnail_number = 1
+# sphinx_gallery_thumbnail_number = 2
