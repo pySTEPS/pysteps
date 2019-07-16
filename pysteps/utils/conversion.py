85f41a1ca638f0afa591a82aa96b4004e1c39879
ned <daniele.nerini@meteoswiss.ch>
2019-07-16 14:27:13 +0200

    Improve syntax

--- pysteps/utils/conversion.py
+++ pysteps/utils/conversion.py
@@ -22,6 +22,7 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 from . import transformation
 
+
 def to_rainrate(R, metadata, a=None, b=None):
     """Convert to rain rate [mm/h].
 
@@ -37,7 +38,7 @@ def to_rainrate(R, metadata, a=None, b=None):
         Additionally, in case of conversion to/from reflectivity units, the
         zr_a and zr_b attributes are also required, but only if a=b=None.
     a,b : float, optional
-        The a and b coefficients of the Z-R relationship.
+        The a and b coefficients of the Z-R relationship (Z = a*R^b).
 
     Returns
     -------
@@ -57,7 +58,7 @@ def to_rainrate(R, metadata, a=None, b=None):
 
             R, metadata = transformation.dB_transform(R, metadata, inverse=True)
 
-        elif metadata["transform"] in  ["BoxCox", "log"]:
+        elif metadata["transform"] in ["BoxCox", "log"]:
 
             R, metadata = transformation.boxcox_transform(R, metadata, inverse=True)
 
@@ -73,36 +74,35 @@ def to_rainrate(R, metadata, a=None, b=None):
 
             raise ValueError("Unknown transformation %s" % metadata["transform"])
 
-
     if metadata["unit"] == "mm/h":
 
         pass
 
     elif metadata["unit"] == "mm":
 
-        threshold = metadata["threshold"] # convert the threshold, too
-        zerovalue = metadata["zerovalue"] # convert the zerovalue, too
+        threshold = metadata["threshold"]  # convert the threshold, too
+        zerovalue = metadata["zerovalue"]  # convert the zerovalue, too
 
-        R = R/float(metadata["accutime"])*60.0
-        threshold = threshold/float(metadata["accutime"])*60.0
-        zerovalue = zerovalue/float(metadata["accutime"])*60.0
+        R = R / float(metadata["accutime"]) * 60.0
+        threshold = threshold / float(metadata["accutime"]) * 60.0
+        zerovalue = zerovalue / float(metadata["accutime"]) * 60.0
 
         metadata["threshold"] = threshold
         metadata["zerovalue"] = zerovalue
 
     elif metadata["unit"] == "dBZ":
 
-        threshold = metadata["threshold"] # convert the threshold, too
-        zerovalue = metadata["zerovalue"] # convert the zerovalue, too
+        threshold = metadata["threshold"]  # convert the threshold, too
+        zerovalue = metadata["zerovalue"]  # convert the zerovalue, too
 
         # Z to R
         if a is None:
             a = metadata.get("zr_a", 316.0)
         if b is None:
             b = metadata.get("zr_b", 1.5)
-        R = (R/a)**(1.0/b)
-        threshold = (threshold/a)**(1.0/b)
-        zerovalue = (zerovalue/a)**(1.0/b)
+        R = (R / a) ** (1.0 / b)
+        threshold = (threshold / a) ** (1.0 / b)
+        zerovalue = (zerovalue / a) ** (1.0 / b)
 
         metadata["zr_a"] = a
         metadata["zr_b"] = b
@@ -110,11 +110,15 @@ def to_rainrate(R, metadata, a=None, b=None):
         metadata["zerovalue"] = zerovalue
 
     else:
-        raise ValueError("Cannot convert unit %s and transform %s to mm/h" % (metadata["unit"], metadata["transform"]))
+        raise ValueError(
+            "Cannot convert unit %s and transform %s to mm/h"
+            % (metadata["unit"], metadata["transform"])
+        )
 
     metadata["unit"] = "mm/h"
 
     return R, metadata
+
 
 def to_raindepth(R, metadata, a=None, b=None):
     """Convert to rain depth [mm].
@@ -131,7 +135,7 @@ def to_raindepth(R, metadata, a=None, b=None):
         Additionally, in case of conversion to/from reflectivity units, the
         zr_a and zr_b attributes are also required, but only if a=b=None.
     a,b : float, optional
-        The a and b coefficients of the Z-R relationship.
+        The a and b coefficients of the Z-R relationship (Z = a*R^b).
 
     Returns
     -------
@@ -172,29 +176,29 @@ def to_raindepth(R, metadata, a=None, b=None):
 
     elif metadata["unit"] == "mm/h":
 
-        threshold = metadata["threshold"] # convert the threshold, too
-        zerovalue = metadata["zerovalue"] # convert the zerovalue, too
+        threshold = metadata["threshold"]  # convert the threshold, too
+        zerovalue = metadata["zerovalue"]  # convert the zerovalue, too
 
-        R = R/60.0*metadata["accutime"]
-        threshold = threshold/60.0*metadata["accutime"]
-        zerovalue = zerovalue/60.0*metadata["accutime"]
+        R = R / 60.0 * metadata["accutime"]
+        threshold = threshold / 60.0 * metadata["accutime"]
+        zerovalue = zerovalue / 60.0 * metadata["accutime"]
 
         metadata["threshold"] = threshold
         metadata["zerovalue"] = zerovalue
 
     elif metadata["unit"] == "dBZ":
 
-        threshold = metadata["threshold"] # convert the threshold, too
-        zerovalue = metadata["zerovalue"] # convert the zerovalue, too
+        threshold = metadata["threshold"]  # convert the threshold, too
+        zerovalue = metadata["zerovalue"]  # convert the zerovalue, too
 
         # Z to R
         if a is None:
             a = metadata.get("zr_a", 316.0)
         if b is None:
             b = metadata.get("zr_b", 1.5)
-        R = (R/a)**(1.0/b)/60.0*metadata["accutime"]
-        threshold = (threshold/a)**(1.0/b)/60.0*metadata["accutime"]
-        zerovalue = (zerovalue/a)**(1.0/b)/60.0*metadata["accutime"]
+        R = (R / a) ** (1.0 / b) / 60.0 * metadata["accutime"]
+        threshold = (threshold / a) ** (1.0 / b) / 60.0 * metadata["accutime"]
+        zerovalue = (zerovalue / a) ** (1.0 / b) / 60.0 * metadata["accutime"]
 
         metadata["zr_a"] = a
         metadata["zr_b"] = b
@@ -202,11 +206,15 @@ def to_raindepth(R, metadata, a=None, b=None):
         metadata["zerovalue"] = zerovalue
 
     else:
-        raise ValueError("Cannot convert unit %s and transform %s to mm" % (metadata["unit"], metadata["transform"]))
+        raise ValueError(
+            "Cannot convert unit %s and transform %s to mm"
+            % (metadata["unit"], metadata["transform"])
+        )
 
     metadata["unit"] = "mm"
 
     return R, metadata
+
 
 def to_reflectivity(R, metadata, a=None, b=None):
     """Convert to reflectivity [dBZ].
@@ -223,7 +231,7 @@ def to_reflectivity(R, metadata, a=None, b=None):
         Additionally, in case of conversion to/from reflectivity units, the
         zr_a and zr_b attributes are also required, but only if a=b=None.
     a,b : float, optional
-        The a and b coefficients of the Z-R relationship.
+        The a and b coefficients of the Z-R relationship (Z = a*R^b).
 
     Returns
     -------
@@ -267,9 +275,9 @@ def to_reflectivity(R, metadata, a=None, b=None):
         if b is None:
             b = metadata.get("zr_b", 1.5)
 
-        R = a*R**b
-        metadata["threshold"] = a*metadata["threshold"]**b
-        metadata["zerovalue"] = a*metadata["zerovalue"]**b
+        R = a * R ** b
+        metadata["threshold"] = a * metadata["threshold"] ** b
+        metadata["zerovalue"] = a * metadata["zerovalue"] ** b
 
         # Z to dBZ
         R, metadata = transformation.dB_transform(R, metadata)
@@ -284,9 +292,9 @@ def to_reflectivity(R, metadata, a=None, b=None):
             a = metadata.get("zr_a", 316.0)
         if b is None:
             b = metadata.get("zr_b", 1.5)
-        R = a*R**b
-        metadata["threshold"] = a*metadata["threshold"]**b
-        metadata["zerovalue"] = a*metadata["zerovalue"]**b
+        R = a * R ** b
+        metadata["threshold"] = a * metadata["threshold"] ** b
+        metadata["zerovalue"] = a * metadata["zerovalue"] ** b
 
         # Z to dBZ
         R, metadata = transformation.dB_transform(R, metadata)
@@ -298,7 +306,10 @@ def to_reflectivity(R, metadata, a=None, b=None):
 
     else:
 
-        raise ValueError("Cannot convert unit %s and transform %s to mm/h" % (metadata["unit"], metadata["transform"]))
+        raise ValueError(
+            "Cannot convert unit %s and transform %s to mm/h"
+            % (metadata["unit"], metadata["transform"])
+        )
 
     metadata["unit"] = "dBZ"
 
