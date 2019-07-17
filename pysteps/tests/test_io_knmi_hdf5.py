817a88347637ac49d51bea5c82da5cd61acf4e85
ned <daniele.nerini@meteoswiss.ch>
2019-07-17 11:07:42 +0200

    Test only if optional dependency is installed

--- pysteps/tests/test_io_knmi_hdf5.py
+++ pysteps/tests/test_io_knmi_hdf5.py
@@ -7,45 +7,53 @@ import pytest
 import pysteps
 from pysteps.tests.helpers import smart_assert
 
+try:
+    import h5py
 
-def test_io_import_knmi_hdf5_shape():
-    """Test the importer KNMI HDF5."""
-    root_path = pysteps.rcparams.data_sources["knmi"]["root_path"]
-    filename = os.path.join(root_path, "2010/08",
-                            "RAD_NL25_RAP_5min_201008260000.h5")
-    R, _, _ = pysteps.io.import_knmi_hdf5(filename)
-    assert R.shape == (765, 700)
+    h5py_imported = True
+except ImportError:
+    h5py_imported = False
 
+if h5py_imported:
 
-# test_metadata: list of (variable,expected, tolerance) tuples
-
-expected_proj1 = ('+proj=stere +lat_0=90 +lon_0=0.0 '
-                  '+lat_ts=60.0 +a=6378.137 +b=6356.752 +x_0=0 '
-                  '+y_0=0')
-
-test_metadata = [
-    ('projection', expected_proj1, None),
-    ('x1', 0.0, 1e-10),
-    ('y1', -3649950.360247753, 1e-10),
-    ('x2', 699984.2646331593, 1e-10),
-    ('y2', -4415038.179210632, 1e-10),
-    ('xpixelsize', 1000.0, 1e-10),
-    ('xpixelsize', 1000.0, 1e-10),
-    ('accutime', 5.0, 1e-10),
-    ('yorigin', 'upper', None),
-    ('unit', 'mm', None),
-    ('institution', 'KNMI - Royal Netherlands Meteorological Institute', None),
-    ('transform', None, None),
-    ('zerovalue', 0.0, 1e-10),
-    ('threshold', 0.01, 1e-10),
-]
+    def test_io_import_knmi_hdf5_shape():
+        """Test the importer KNMI HDF5."""
+        root_path = pysteps.rcparams.data_sources["knmi"]["root_path"]
+        filename = os.path.join(root_path, "2010/08",
+                                "RAD_NL25_RAP_5min_201008260000.h5")
+        R, _, _ = pysteps.io.import_knmi_hdf5(filename)
+        assert R.shape == (765, 700)
 
 
-@pytest.mark.parametrize("variable,expected,tolerance", test_metadata)
-def test_io_import_knmi_hdf5_metadata(variable, expected, tolerance):
-    """Test the importer KNMI HDF5."""
-    root_path = pysteps.rcparams.data_sources["knmi"]["root_path"]
-    filename = os.path.join(root_path, "2010/08",
-                            "RAD_NL25_RAP_5min_201008260000.h5")
-    _, _, metadata = pysteps.io.import_knmi_hdf5(filename)
-    smart_assert(metadata[variable], expected, tolerance)
+    # test_metadata: list of (variable,expected, tolerance) tuples
+
+    expected_proj1 = ('+proj=stere +lat_0=90 +lon_0=0.0 '
+                      '+lat_ts=60.0 +a=6378.137 +b=6356.752 +x_0=0 '
+                      '+y_0=0')
+
+    test_metadata = [
+        ('projection', expected_proj1, None),
+        ('x1', 0.0, 1e-10),
+        ('y1', -3649950.360247753, 1e-10),
+        ('x2', 699984.2646331593, 1e-10),
+        ('y2', -4415038.179210632, 1e-10),
+        ('xpixelsize', 1000.0, 1e-10),
+        ('xpixelsize', 1000.0, 1e-10),
+        ('accutime', 5.0, 1e-10),
+        ('yorigin', 'upper', None),
+        ('unit', 'mm', None),
+        ('institution', 'KNMI - Royal Netherlands Meteorological Institute', None),
+        ('transform', None, None),
+        ('zerovalue', 0.0, 1e-10),
+        ('threshold', 0.01, 1e-10),
+    ]
+
+
+    @pytest.mark.parametrize("variable,expected,tolerance", test_metadata)
+    def test_io_import_knmi_hdf5_metadata(variable, expected, tolerance):
+        """Test the importer KNMI HDF5."""
+        root_path = pysteps.rcparams.data_sources["knmi"]["root_path"]
+        filename = os.path.join(root_path, "2010/08",
+                                "RAD_NL25_RAP_5min_201008260000.h5")
+        _, _, metadata = pysteps.io.import_knmi_hdf5(filename)
+        smart_assert(metadata[variable], expected, tolerance)
