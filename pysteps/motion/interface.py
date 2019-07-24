bafcaee3f07bbd7434ea2b00c18a003ce956bad4
ned <daniele.nerini@meteoswiss.ch>
2019-07-24 08:35:56 +0200

    Add docstrings to interface

--- pysteps/motion/interface.py
+++ pysteps/motion/interface.py
@@ -1,3 +1,27 @@
+"""
+pysteps.motion.interface
+========================
+
+Interface for the motion module. It returns a callable optical flow routine for
+computing the motion field.
+
+The methods in the motion module implement the following interface:
+
+    ``motion_method(precip, **keywords)``
+
+where precip is a (T,m,n) array containing a sequence of T two-dimensional input
+images of shape (m,n). The first dimension represents the images time dimension
+and the value of T depends on the type of the method.
+
+The output is a three-dimensional array (2,m,n) containing the dense x- and
+y-components of the motion field in units of pixels / timestep as given by the
+input array R.
+
+.. autosummary::
+    :toctree: ../generated/
+
+    get_method
+"""
 import numpy as np
 
 from pysteps.motion.constant import constant
