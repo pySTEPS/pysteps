bbbef271a3e522425de585922279a62d92a1dac6
aperezhortal <andresperezcba@gmail.com>
2019-07-27 16:41:14 +0000

    Fix bug in if conditions

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -2,11 +2,13 @@
 
 # Load pysteps-data
 # -----------------
+echo "Download pysteps example data"
 export PYSTEPS_BUILD_DIR="$( pwd )"
 git clone https://github.com/pySTEPS/pysteps-data.git $PYSTEPS_BUILD_DIR/pysteps-data
 python ci/create_pystepsrc_file.py
 export PYSTEPSRC=$PYSTEPS_BUILD_DIR/pystepsrc.travis
 
 # Run tests
+echo "Run test suite"
 cd ~
 pytest --pyargs pysteps --cov=pysteps -ra;
