e244088a324ff7129a97a3701e2ba2a377ecaf30
aperezhortal <andresperezcba@gmail.com>
2019-07-27 17:26:39 +0000

    Fix scripts' paths

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -3,9 +3,8 @@
 # Load pysteps-data
 # -----------------
 echo "Download pysteps example data"
-export PYSTEPS_BUILD_DIR="$( pwd )"
 git clone https://github.com/pySTEPS/pysteps-data.git $PYSTEPS_BUILD_DIR/pysteps-data
-python ci/create_pystepsrc_file.py
+python $PYSTEPS_BUILD_DIR/ci/create_pystepsrc_file.py
 export PYSTEPSRC=$PYSTEPS_BUILD_DIR/pystepsrc.travis
 
 # Run tests
