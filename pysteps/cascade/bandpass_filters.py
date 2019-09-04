b77bca116b7b14938589a05d4e516a3a8a16a141
Carlos Velasco <carlos.velasco@bom.gov.au>
2019-09-04 12:20:45 +0000

    PEP8 changes

--- pysteps/cascade/bandpass_filters.py
+++ pysteps/cascade/bandpass_filters.py
@@ -32,8 +32,8 @@ key-value pairs:
 
 where r = int(max(N, M)/2)+1
 
-By default, the filter weights are normalized so that for any Fourier wavenumber
-they sum to one.
+By default, the filter weights are normalized so that for any Fourier
+wavenumber they sum to one.
 
 Available filters
 -----------------
@@ -46,6 +46,7 @@ Available filters
 """
 
 import numpy as np
+
 
 def filter_uniform(shape, n):
     """A dummy filter with one frequency band covering the whole domain. The
@@ -63,18 +64,19 @@ def filter_uniform(shape, n):
     result = {}
 
     try:
-        M,N = shape
+        M, N = shape
     except TypeError:
-        M,N = (shape, shape)
+        M, N = (shape, shape)
 
     r_max = int(max(N, M)/2)+1
 
-    result["weights_1d"]    = np.ones((1, r_max))
-    result["weights_2d"]    = np.ones((1, M, int(N/2)+1))
+    result["weights_1d"] = np.ones((1, r_max))
+    result["weights_2d"] = np.ones((1, M, int(N/2)+1))
     result["central_freqs"] = None
     result["central_wavenumbers"] = None
 
     return result
+
 
 def filter_gaussian(shape, n, l_0=3, gauss_scale=0.5, gauss_scale_0=0.5, d=1.0,
                     normalize=True):
@@ -92,16 +94,16 @@ def filter_gaussian(shape, n, l_0=3, gauss_scale=0.5, gauss_scale_0=0.5, d=1.0,
         Central frequency of the second band (the first band is always centered
         at zero).
     gauss_scale : float
-        Optional scaling prameter. Proportional to the standard deviation of the
-        Gaussian weight functions.
+        Optional scaling prameter. Proportional to the standard deviation of
+        the Gaussian weight functions.
     gauss_scale_0 : float
         Optional scaling parameter for the Gaussian function corresponding to
         the first frequency band.
     d : scalar, optional
         Sample spacing (inverse of the sampling rate). Defaults to 1.
     normalize : bool
-        If True, normalize the weights so that for any given wavenumber they sum
-        to one.
+        If True, normalize the weights so that for any given wavenumber
+        they sum to one.
 
     Returns
     -------
@@ -118,9 +120,9 @@ def filter_gaussian(shape, n, l_0=3, gauss_scale=0.5, gauss_scale_0=0.5, d=1.0,
         raise ValueError("n must be greater than 2")
 
     try:
-        M,N = shape
+        M, N = shape
     except TypeError:
-        M,N = (shape, shape)
+        M, N = (shape, shape)
 
     rx = np.s_[:int(N/2)+1]
 
@@ -129,7 +131,7 @@ def filter_gaussian(shape, n, l_0=3, gauss_scale=0.5, gauss_scale_0=0.5, d=1.0,
     else:
         ry = np.s_[-int(M/2):int(M/2)]
 
-    Y,X = np.ogrid[ry, rx]
+    Y, X = np.ogrid[ry, rx]
     dy = int(M/2) if M % 2 == 0 else int(M/2)+1
     R = np.roll(np.sqrt(X*X + Y*Y), dy, axis=0)
 
@@ -137,13 +139,13 @@ def filter_gaussian(shape, n, l_0=3, gauss_scale=0.5, gauss_scale_0=0.5, d=1.0,
     r_max = int(L/2)+1
     r = np.arange(r_max)
 
-    wfs,cwn = _gaussweights_1d(L, n, l_0=l_0, gauss_scale=gauss_scale,
-                               gauss_scale_0=gauss_scale_0)
+    wfs, cwn = _gaussweights_1d(L, n, l_0=l_0, gauss_scale=gauss_scale,
+                                gauss_scale_0=gauss_scale_0)
 
     w = np.empty((n, r_max))
     W = np.empty((n, M, int(N/2)+1))
 
-    for i,wf in enumerate(wfs):
+    for i, wf in enumerate(wfs):
         w[i, :] = wf(r)
         W[i, :, :] = wf(R)
 
@@ -151,30 +153,32 @@ def filter_gaussian(shape, n, l_0=3, gauss_scale=0.5, gauss_scale_0=0.5, d=1.0,
         w_sum = np.sum(w, axis=0)
         W_sum = np.sum(W, axis=0)
         for k in range(W.shape[0]):
-            w[k, :]    /= w_sum
+            w[k, :] /= w_sum
             W[k, :, :] /= W_sum
 
     result = {}
     result["weights_1d"] = w
     result["weights_2d"] = W
-    
+
     cwn = np.array(cwn)
     result["central_wavenumbers"] = cwn
-    
+
     # Compute frequencies
     central_freqs = 1.0*cwn/L
     central_freqs[0] = 1.0/L
-    central_freqs[-1] = 0.5 # Nyquist freq
+    central_freqs[-1] = 0.5  # Nyquist freq
     central_freqs = 1.0*d*central_freqs
     result["central_freqs"] = central_freqs
 
     return result
 
+
 def _gaussweights_1d(l, n, l_0=3, gauss_scale=0.5, gauss_scale_0=0.5):
     e = pow(0.5*l/l_0, 1.0/(n-2))
     r = [(l_0*pow(e, k-1), l_0*pow(e, k)) for k in range(1, n-1)]
 
-    f = lambda x,s: np.exp(-x**2.0 / (2.0*s**2.0))
+    f = lambda x, s: np.exp(-x**2.0 / (2.0*s**2.0))
+
     def log_e(x):
         if len(np.shape(x)) > 0:
             res = np.empty(x.shape)
@@ -197,18 +201,19 @@ def _gaussweights_1d(l, n, l_0=3, gauss_scale=0.5, gauss_scale_0=0.5):
         def __call__(self, x):
             return f(log_e(x) - self.c, self.s)
 
-    weight_funcs  = []
+    weight_funcs = []
     central_wavenumbers = [0.0]
 
     s = gauss_scale
     weight_funcs.append(gaussfunc(0.0, gauss_scale_0))
 
-    for i,ri in enumerate(r):
+    for i, ri in enumerate(r):
         rc = log_e(ri[0])
         weight_funcs.append(gaussfunc(rc, s))
         central_wavenumbers.append(ri[0])
 
     gf = gaussfunc(log_e(l/2), s)
+
     def g(x):
         res = np.ones(x.shape)
         mask = x <= l/2
