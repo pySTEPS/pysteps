b37de81fc68f26ebfd3f7ffb2ba4ecceffd12b9b
Carlos Velasco <carlos.velasco@bom.gov.au>
2019-09-04 14:26:10 +0000

    More PEP8 improvements

--- pysteps/visualization/utils.py
+++ pysteps/visualization/utils.py
@@ -13,7 +13,8 @@ Miscellaneous utility functions for the visualization module.
     reproject_geodata
 """
 import numpy as np
-from pysteps.exceptions import MissingOptionalDependency, UnsupportedSomercProjection
+from pysteps.exceptions import MissingOptionalDependency
+from pysteps.exceptions import UnsupportedSomercProjection
 
 try:
     import cartopy.crs as ccrs
@@ -26,6 +27,7 @@ try:
 except ImportError:
     pyproj_imported = False
 
+
 def parse_proj4_string(proj4str):
     """Construct a dictionary from a PROJ.4 projection string.
 
@@ -37,8 +39,8 @@ def parse_proj4_string(proj4str):
     Returns
     -------
     out : dict
-      Dictionary, where keys and values are parsed from the projection parameter
-      tokens beginning with '+'.
+      Dictionary, where keys and values are parsed from the projection
+      parameter tokens beginning with '+'.
 
     """
     tokens = proj4str.split('+')
@@ -46,10 +48,11 @@ def parse_proj4_string(proj4str):
     result = {}
     for t in tokens[1:]:
         if '=' in t:
-            k,v = t.split('=')
+            k, v = t.split('=')
             result[k] = v.strip()
 
     return result
+
 
 def proj4_to_basemap(proj4str):
     """Convert a PROJ.4 projection string into a dictionary that can be expanded
@@ -69,10 +72,11 @@ def proj4_to_basemap(proj4str):
     pdict = parse_proj4_string(proj4str)
     odict = {}
 
-    for k,v in list(pdict.items()):
+    for k, v in list(pdict.items()):
         if k == "proj":
             # TODO: Make sure that the proj.4 projection type is in all cases
-            # mapped to the corresponding (or closest matching) Basemap projection.
+            # mapped to the corresponding (or closest matching) Basemap
+            # projection.
             if v not in ["latlon", "latlong", "lonlat", "longlat"]:
                 odict["projection"] = v
             else:
@@ -92,6 +96,7 @@ def proj4_to_basemap(proj4str):
 
     return odict
 
+
 def proj4_to_cartopy(proj4str):
     """Convert a PROJ.4 projection string into a Cartopy coordinate reference
     system (crs) object.
@@ -109,8 +114,8 @@ def proj4_to_cartopy(proj4str):
     """
     if not cartopy_imported:
         raise MissingOptionalDependency(
-            "cartopy package is required for proj4_to_cartopy function utility "
-            "but it is not installed")
+            "cartopy package is required for proj4_to_cartopy function "
+            "utility but it is not installed")
 
     if not pyproj_imported:
         raise MissingOptionalDependency(
@@ -134,9 +139,9 @@ def proj4_to_cartopy(proj4str):
     km_std = {"lat_1": "lat_1",
               "lat_2": "lat_2"}
 
-    kw_proj  = {}
+    kw_proj = {}
     kw_globe = {}
-    kw_std   = {}
+    kw_std = {}
 
     for s in proj.srs.split('+'):
         s = s.split('=')
@@ -146,7 +151,8 @@ def proj4_to_cartopy(proj4str):
         v = s[1].strip()
         try:
             v = float(v)
-        except:
+        except Exception as err:
+            print(err)
             pass
 
         if k == "proj":
@@ -165,7 +171,8 @@ def proj4_to_cartopy(proj4str):
             elif v == "aea":
                 cl = ccrs.AlbersEqualArea
             elif v == "somerc":
-                raise UnsupportedSomercProjection("unsupported projection: somerc")
+                raise UnsupportedSomercProjection("unsupported projection:"
+                                                  " somerc")
             else:
                 raise ValueError("unsupported projection: %s" % v)
         elif k in km_proj:
@@ -187,15 +194,16 @@ def proj4_to_cartopy(proj4str):
 
     return cl(globe=globe, **kw_proj)
 
+
 def reproject_geodata(geodata, t_proj4str, return_grid=None):
     """
     Reproject geodata and optionally create a grid in a new projection.
-    
+
     Parameters
     ----------
     geodata : dictionary
         Dictionary containing geographical information about the field.
-        It must contain the attributes projection, x1, x2, y1, y2, xpixelsize, 
+        It must contain the attributes projection, x1, x2, y1, y2, xpixelsize,
         ypixelsize, as defined in the documentation of pysteps.io.importers.
     t_proj4str: str
         The target PROJ.4-compatible projection string (fallback).
@@ -205,34 +213,35 @@ def reproject_geodata(geodata, t_proj4str, return_grid=None):
         return_grid='coords' returns the centers of projected grid points,
         return_grid='quadmesh' returns the coordinates of the quadrilaterals
         (e.g. to be used by pcolormesh).
-    
+
     Returns
     -------
     geodata : dictionary
-        Dictionary containing the reprojected geographical information 
+        Dictionary containing the reprojected geographical information
         and optionally the required X_grid and Y_grid. \n
-        It also includes a fixed boolean attribute regular_grid=False to indicate
+        It also includes a fixed boolean attribute
+        regular_grid=False to indicate
         that the reprojected grid has no regular spacing.
     """
     if not pyproj_imported:
         raise MissingOptionalDependency(
-            "pyproj package is required for reproject_geodata function utility "
-            "but it is not installed")
-    
+            "pyproj package is required for reproject_geodata function utility"
+            " but it is not installed")
+
     geodata = geodata.copy()
     s_proj4str = geodata["projection"]
     extent = (geodata["x1"], geodata["x2"], geodata["y1"], geodata["y2"])
     shape = (int((geodata["y2"]-geodata["y1"])/geodata["ypixelsize"]),
-            int((geodata["x2"]-geodata["x1"])/geodata["xpixelsize"]))
-    
+             int((geodata["x2"]-geodata["x1"])/geodata["xpixelsize"]))
+
     s_srs = pyproj.Proj(s_proj4str)
     t_srs = pyproj.Proj(t_proj4str)
-    
+
     x1 = extent[0]
     x2 = extent[1]
     y1 = extent[2]
     y2 = extent[3]
-    
+
     # Reproject grid on fall-back projection
     if return_grid is not None:
         if return_grid == "coords":
@@ -243,17 +252,17 @@ def reproject_geodata(geodata, t_proj4str, return_grid=None):
             x_coord = np.linspace(x1, x2, shape[1] + 1)
         else:
             raise ValueError("unknown return_grid value %s" % return_grid)
-            
+
         X, Y = np.meshgrid(x_coord, y_coord)
-        
-        X, Y = pyproj.transform(s_srs, t_srs, X.flatten(), Y.flatten())    
+
+        X, Y = pyproj.transform(s_srs, t_srs, X.flatten(), Y.flatten())
         X = X.reshape((y_coord.size, x_coord.size))
         Y = Y.reshape((y_coord.size, x_coord.size))
-    
+
     # Reproject extent on fall-back projection
     x1, y1 = pyproj.transform(s_srs, t_srs, x1, y1)
     x2, y2 = pyproj.transform(s_srs, t_srs, x2, y2)
-    
+
     # update geodata
     geodata["projection"] = t_proj4str
     geodata["x1"] = x1
@@ -265,5 +274,5 @@ def reproject_geodata(geodata, t_proj4str, return_grid=None):
     geodata["ypixelsize"] = None
     geodata["X_grid"] = X
     geodata["Y_grid"] = Y
-    
+
     return geodata
