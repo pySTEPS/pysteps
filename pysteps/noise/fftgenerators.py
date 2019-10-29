d53f1e70fe1acc65d2b92e5f75d33d750a00af25
Seppo Pulkkinen <pulkkins@gmail.com>
2019-10-29 16:59:22 -0600

    Implement normalization of the noise field in the spectral domain

--- pysteps/noise/fftgenerators.py
+++ pysteps/noise/fftgenerators.py
@@ -398,9 +398,9 @@ def generate_noise_2d_fft_filter(F, randstate=None, seed=None, fft_method=None,
     else:
         N = fN
         N[0, 0] = 0.0
-        #var = np.sum(np.abs(N**2)) + np.sum(np.abs(N[:, 1:]**2))
-        #var /= input_shape[0]**2 * input_shape[1]**2
-        #N /= np.sqrt(var)
+        var = np.sum(np.abs(N**2)) + np.sum(np.abs(N[:, 1:]**2))
+        var /= input_shape[0]**2 * input_shape[1]**2
+        N /= np.sqrt(var)
 
     return N
 
