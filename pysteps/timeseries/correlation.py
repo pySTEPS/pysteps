13d20b7de23530a9683d6585f9a2f57e07ddb432
Seppo Pulkkinen <pulkkins@gmail.com>
2019-11-04 14:50:25 -0700

    More generic implementation of temporal_autocorrelation and use lowercase variable names

--- pysteps/timeseries/correlation.py
+++ pysteps/timeseries/correlation.py
@@ -13,28 +13,27 @@ two-dimensional fields.
 
 import numpy as np
 
-def temporal_autocorrelation(X, domain="spatial", X_shape=None, MASK=None):
+def temporal_autocorrelation(x, domain="spatial", x_shape=None, mask=None):
     """Compute lag-l autocorrelation coefficients gamma_l, l=1,2,...,n-1, for a
-    time series of n two-dimensional input fields in spatial or spectral domain.
+    time series of n inputs of arbitraty dimension in spatial or spectral domain.
 
     Parameters
     ----------
-    X : array_like
-        Two-dimensional array of shape (n, w, h) containing a time series of n
-        two-dimensional fields of shape (w, h). The input fields are assumed to
-        be in increasing order with respect to time, and the time step is assumed
-        to be regular (i.e. no missing data). X is required to have
-        finite values.
+    x : array_like
+        Array of shape (n, ...) containing a time series of n inputs of arbitrary
+        dimension. The inputs are assumed to be in increasing order with respect
+        to time, and the time step is assumed to be regular. x is required to
+        have finite values.
     domain : {"spatial", "spectral"}
-        The domain of the arrays X. If domain is "spectral", the arrays are
-        assumed to represent the FFTs of the original arrays.
-    X_shape : tuple
-        The shape of the original arrays in the spatial domain before the FFT.
-        Required if domain is "spectral".
-    MASK : array_like
-        Optional mask to use for computing the correlation coefficients. Pixels
-        with MASK==False are excluded from the computations. Applicable if domain
-        is "spatial".
+        The domain of the time series x. If domain is "spectral", the elements
+        of x are assumed to represent the FFTs of the original elements.
+    x_shape : tuple
+        The shape of the original arrays in the spatial domain before applying
+        the FFT. Required if domain is "spectral".
+    mask : array_like
+        Optional mask to use for computing the correlation coefficients. Input
+        elements with mask==False are excluded from the computations. Applicable
+        if domain is "spatial".
 
     Returns
     -------
@@ -42,40 +41,47 @@ def temporal_autocorrelation(X, domain="spatial", X_shape=None, MASK=None):
         Array of length n-1 containing the temporal autocorrelation coefficients
         for time lags l=1,2,...,n-1.
 
-    """
-    if len(X.shape) != 3:
-        raise ValueError("the input X is not three-dimensional array")
-    if MASK is not None and MASK.shape != X.shape[1:3]:
-        raise ValueError("dimension mismatch between X and MASK: X.shape=%s, MASK.shape=%s" % \
-                         (str(X.shape), str(MASK.shape)))
-    if np.any(~np.isfinite(X)):
-        raise ValueError("X contains non-finite values")
+    Notes
+    -----
+    Computation of correlation coefficients in the spectral domain is currently
+    implemented only for two-dimensional fields.
 
-    if domain == "spatial" and MASK is None:
-        MASK = np.ones((X.shape[1], X.shape[2]), dtype=bool)
+    """
+    if len(x.shape) < 2:
+        raise ValueError("the dimension of x must be >= 2")
+    if len(x.shape) != 3 and domain == "spectral":
+        raise NotImplementedError("with domain == 'spectral', this function has only been implemented for two-dimensional fields")
+    if mask is not None and mask.shape != x.shape[1:]:
+        raise ValueError("dimension mismatch between x and mask: x.shape[1:]=%s, mask.shape=%s" % \
+                         (str(x.shape), str(mask.shape)))
+    if np.any(~np.isfinite(x)):
+        raise ValueError("x contains non-finite values")
+
+    if domain == "spatial" and mask is None:
+        mask = np.ones(x.shape[1:], dtype=bool)
 
     if domain == "spectral":
-        X = X.copy()
-        for k in range(X.shape[0]):
-            X[k, 0, 0] = 0.0
+        x = x.copy()
+        for k in range(x.shape[0]):
+            x[k, np.zeros(x.shape[1:], dtype=int)] = 0.0
 
     gamma = []
-    for k in range(X.shape[0] - 1):
+    for k in range(x.shape[0] - 1):
         if domain == "spatial":
-            cc = np.corrcoef(X[-1, :, :][MASK], X[-(k+2), :, :][MASK])[0, 1]
+            cc = np.corrcoef(x[-1, :][mask], x[-(k+2), :][mask])[0, 1]
         else:
-            n = np.real(np.sum(X[-1, :, :]*np.conj(X[-(k+2), :, :])))
-            d1 = np.sum(np.abs(X[-1, :, :])**2)
-            d2 = np.sum(np.abs(X[-(k+2), :, :])**2)
+            n = np.real(np.sum(x[-1, :, :]*np.conj(x[-(k+2), :, :])))
+            d1 = np.sum(np.abs(x[-1, :, :])**2)
+            d2 = np.sum(np.abs(x[-(k+2), :, :])**2)
 
-            if X_shape[1] % 2 == 0:
-                n += np.real(np.sum(X[-1, :, :][:, 1:-1]*np.conj(X[-(k+2), :, :][:, 1:-1])))
-                d1 += np.sum(np.abs(X[-1, :, :][:, 1:-1])**2)
-                d2 += np.sum(np.abs(X[-(k+2), :, :][:, 1:-1])**2)
+            if x_shape[1] % 2 == 0:
+                n += np.real(np.sum(x[-1, :, :][:, 1:-1]*np.conj(x[-(k+2), :, :][:, 1:-1])))
+                d1 += np.sum(np.abs(x[-1, :, :][:, 1:-1])**2)
+                d2 += np.sum(np.abs(x[-(k+2), :, :][:, 1:-1])**2)
             else:
-                n += np.real(np.sum(X[-1, :, :][:, 1:]*np.conj(X[-(k+2), :, :][:, 1:])))
-                d1 += np.sum(np.abs(X[-1, :, :][:, 1:])**2)
-                d2 += np.sum(np.abs(X[-(k+2), :, :][:, 1:])**2)
+                n += np.real(np.sum(x[-1, :, :][:, 1:]*np.conj(x[-(k+2), :, :][:, 1:])))
+                d1 += np.sum(np.abs(x[-1, :, :][:, 1:])**2)
+                d2 += np.sum(np.abs(x[-(k+2), :, :][:, 1:])**2)
 
             cc = n / np.sqrt(d1*d2)
         gamma.append(cc)
