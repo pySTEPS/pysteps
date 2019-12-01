34e82d33cfb3534aac0112ccb81cd74b93a3b54c
Seppo Pulkkinen <pulkkins@gmail.com>
2019-12-01 00:47:19 -0700

    Implement more general autocorrelation computation and use lowercase variable names

--- pysteps/timeseries/correlation.py
+++ pysteps/timeseries/correlation.py
@@ -14,45 +14,43 @@ two-dimensional fields.
 import numpy as np
 
 
-def temporal_autocorrelation(X, MASK=None):
+def temporal_autocorrelation(x, mask=None):
     """Compute lag-l autocorrelation coefficients gamma_l, l=1,2,...,n-1, for a
-    time series of n two-dimensional input fields.
+    time series of n inputs of arbitrary dimension.
 
     Parameters
     ----------
-    X : array_like
-      Two-dimensional array of shape (n, w, h) containing a time series of n
-      two-dimensional fields of shape (w, h). The input fields are assumed to
-      be in increasing order with respect to time, and the time step is assumed
-      to be regular (i.e. no missing data). X is required to have
-      finite values.
-    MASK : array_like
-      Optional mask to use for computing the correlation coefficients. Pixels
-      with MASK==False are excluded from the computations.
+    x : array_like
+        Array of shape (n, ...) containing a time series of n inputs of arbitrary
+        dimension. The inputs are assumed to be in increasing order with respect
+        to time, and the time step is assumed to be regular. x is required to
+        have finite values.
+    mask : array_like
+        Optional mask to use for computing the correlation coefficients. Input
+        elements with mask==False are excluded from the computations.
 
     Returns
     -------
-    out : ndarray
-      Array of length n-1 containing the temporal autocorrelation coefficients
-      for time lags l=1,2,...,n-1.
+    out : list
+        List of length n-1 containing the temporal autocorrelation coefficients
+        for time lags l=1,2,...,n-1.
 
     """
-    if len(X.shape) != 3:
-        raise ValueError("the input X is not three-dimensional array")
-    if MASK is not None and MASK.shape != X.shape[1:3]:
-        raise ValueError("dimension mismatch between X and MASK: X.shape=%s, MASK.shape=%s" % \
-                         (str(X.shape), str(MASK.shape)))
-    if np.any(~np.isfinite(X)):
-        raise ValueError("X contains non-finite values")
+    if len(x.shape) < 2:
+        raise ValueError("the dimension of x must be >= 2")
+    if mask is not None and mask.shape != x.shape[1:]:
+        raise ValueError("dimension mismatch between x and mask: x.shape[1:]=%s, mask.shape=%s" % \
+                         (str(x.shape), str(mask.shape)))
+    if np.any(~np.isfinite(x)):
+        raise ValueError("x contains non-finite values")
 
-    gamma = np.empty(X.shape[0]-1)
+    gamma = np.empty(x.shape[0]-1)
 
-    if MASK is None:
-        MASK = np.ones((X.shape[1], X.shape[2]), dtype=bool)
+    if mask is None:
+        mask = np.ones(x.shape[1:], dtype=bool)
 
     gamma = []
-    for k in range(X.shape[0] - 1):
-        gamma.append(np.corrcoef(X[-1, :, :][MASK],
-                                 X[-(k+2), :, :][MASK])[0, 1])
+    for k in range(x.shape[0] - 1):
+        gamma.append(np.corrcoef(x[-1, :][mask], x[-(k+2), :][mask])[0, 1])
 
     return gamma
