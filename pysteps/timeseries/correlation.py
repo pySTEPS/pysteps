835d960a53ddbba4012acd37d7ff9bae512da2bb
Seppo Pulkkinen <pulkkins@gmail.com>
2020-02-04 16:18:52 -0700

    Merge commit from master

--- pysteps/timeseries/correlation.py
+++ pysteps/timeseries/correlation.py
@@ -2,7 +2,8 @@
 pysteps.timeseries.correlation
 ==============================
 
-Methods for computing spatial and temporal correlations from time series data.
+Methods for computing spatial and temporal correlation of time series of
+two-dimensional fields.
 
 .. autosummary::
     :toctree: ../generated/
@@ -11,12 +12,15 @@ Methods for computing spatial and temporal correlations from time series data.
 """
 
 import numpy as np
+from pysteps.utils import spectral
 
 
-def temporal_autocorrelation(x, mask=None):
+def temporal_autocorrelation(x, domain="spatial", x_shape=None, mask=None,
+                             use_full_fft=False):
     """Compute lag-l temporal autocorrelation coefficients
     :math:`\gamma_l=\mbox{corr}(x(t),x(t-l))`, :math:`l=1,2,\dots,n-1`,
-    from a time series :math:`x_1,x_2,\dots,x_n`.
+    from a time series :math:`x_1,x_2,\dots,x_n` of arbitrary dimension in
+    spatial or spectral domain.
 
     Parameters
     ----------
@@ -26,9 +30,20 @@ def temporal_autocorrelation(x, mask=None):
         the time step is assumed to be regular. All inputs are required to have
         finite values. The remaining dimensions after the first one are
         flattened before computing the correlation coefficients.
+    domain : {"spatial", "spectral"}
+        The domain of the time series x. If domain is "spectral", the elements
+        of x are assumed to represent the FFTs of the original elements.
+    x_shape : tuple
+        The shape of the original arrays in the spatial domain before applying
+        the FFT. Required if domain is "spectral".
     mask : array_like
         Optional mask to use for computing the correlation coefficients. Input
-        elements with mask==False are excluded from the computations.
+        elements with mask==False are excluded from the computations. Applicable
+        if domain is "spatial".
+    use_full_fft : bool
+        If True, x represents the full FFTs of the original arrays. Otherwise,
+        the elements of x are assumed to contain only the symmetric part, i.e.
+        in the format returned by numpy.fft.rfft2. Defaults to False.
 
     Returns
     -------
@@ -36,23 +51,38 @@ def temporal_autocorrelation(x, mask=None):
         List of length n-1 containing the temporal autocorrelation coefficients
         :math:`\gamma_i` for time lags :math:`l=1,2,...,n-1`.
 
+    Notes
+    -----
+    Computation of correlation coefficients in the spectral domain is currently
+    implemented only for two-dimensional fields.
+
     """
     if len(x.shape) < 2:
         raise ValueError("the dimension of x must be >= 2")
+    if len(x.shape) != 3 and domain == "spectral":
+        raise NotImplementedError("len(x.shape[1:]) = %d, but with domain == 'spectral', this function has only been implemented for two-dimensional fields" % len(x.shape[1:]))
     if mask is not None and mask.shape != x.shape[1:]:
         raise ValueError("dimension mismatch between x and mask: x.shape[1:]=%s, mask.shape=%s" % \
                          (str(x.shape[1:]), str(mask.shape)))
     if np.any(~np.isfinite(x)):
         raise ValueError("x contains non-finite values")
 
-    gamma = np.empty(x.shape[0]-1)
-
-    if mask is None:
+    if domain == "spatial" and mask is None:
         mask = np.ones(x.shape[1:], dtype=bool)
+
+    if domain == "spectral":
+        x = x.copy()
+        for k in range(x.shape[0]):
+            x[k, np.zeros(x.shape[1:], dtype=int)] = 0.0
 
     gamma = []
     for k in range(x.shape[0] - 1):
-        gamma.append(np.corrcoef(x[-1, :][mask], x[-(k+2), :][mask])[0, 1])
+        if domain == "spatial":
+            cc = np.corrcoef(x[-1, :][mask], x[-(k+2), :][mask])[0, 1]
+        else:
+            cc = spectral.corrcoef(x[-1, :, :], x[-(k+2), :], x_shape,
+                                   use_full_fft=use_full_fft)
+        gamma.append(cc)
 
     return gamma
 
