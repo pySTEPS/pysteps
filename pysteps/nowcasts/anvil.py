47162cfce9d500946422429f399d4d11cf678acd
Seppo Pulkkinen <pulkkins@gmail.com>
2020-03-16 17:10:43 -0600

    Add module docstring

---
+++ pysteps/nowcasts/anvil.py
@@ -0,0 +1,15 @@
+"""
+pysteps.nowcasts.anvil
+======================
+
+Implementation of the autoregressive nowcasting using VIL (ANVIL) nowcasting
+method developed in :cite:`PCHL2020`.
+
+.. autosummary::
+    :toctree: ../generated/
+    
+    forecast
+"""
+
+def forecast():
+    pass
