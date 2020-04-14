4d2a98fd5dcd46975367f422f6358e80e64682a2
ned <daniele.nerini@meteoswiss.ch>
2020-04-14 20:06:58 +0200

    Support MaskedArrays

--- pysteps/utils/images.py
+++ pysteps/utils/images.py
@@ -187,10 +187,16 @@ def morph_opening(input_image, thr, n):
     """Filter out small scale noise on the image by applying a binary
     morphological opening, that is, erosion followed by dilation.
 
+    .. _MaskedArray:\
+        https://docs.scipy.org/doc/numpy/reference/maskedarray.baseclass.html#numpy.ma.MaskedArray
+
+    .. _ndarray:\
+    https://docs.scipy.org/doc/numpy/reference/generated/numpy.ndarray.html
+
     Parameters
     ----------
 
-    input_image : array_like
+    input_image : ndarray_ or MaskedArray_
         Array of shape (m, n) containing the input image.
 
     thr : float
@@ -202,7 +208,7 @@ def morph_opening(input_image, thr, n):
     Returns
     -------
 
-    input_image : array_like
+    input_image : ndarray_ or MaskedArray_
         Array of shape (m,n) containing the filtered image.
     """
     if not CV2_IMPORTED:
@@ -211,8 +217,18 @@ def morph_opening(input_image, thr, n):
             "routine but it is not installed"
         )
 
+    input_image = input_image.copy()
+
+    # masked array
+    to_ndarray = False
+    if not isinstance(input_image, MaskedArray):
+        to_ndarray = True
+        input_image = np.ma.masked_invalid(input_image)
+
+    np.ma.set_fill_value(input_image, input_image.min())
+
     # Convert to binary image
-    field_bin = np.ndarray.astype(input_image > thr, "uint8")
+    field_bin = np.ndarray.astype(input_image.filled() > thr, "uint8")
 
     # Build a structuring element of size n
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (n, n))
@@ -225,5 +241,8 @@ def morph_opening(input_image, thr, n):
 
     # Filter out small isolated pixels based on mask
     input_image[mask] = np.nanmin(input_image)
+
+    if to_ndarray:
+        input_image = np.array(input_image)
 
     return input_image
