72fe8b5908fd99f8f36ed5ef6c6c14234c28f785
Andres Perez Hortal <andresperezcba@gmail.com>
2020-07-14 17:50:33 -0400

    Add precommit configuration

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -145,15 +145,7 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["../_static"]
-
-# html_context = {
-#     'css_files': [
-#        '../_static/pysteps.css',  # override wide tables in RTD theme
-#        ],
-#     }
-
-#def setup(app):
-#   app.add_stylesheet("pysteps.css")
+html_css_files = ["../_static/pysteps.css"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
