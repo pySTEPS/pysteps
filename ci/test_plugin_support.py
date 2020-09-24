0cd844e440203bb1062dc4c512c9121ecc0a937d
Andres Perez Hortal <andresperezcba@gmail.com>
2020-09-24 10:20:20 -0400

    Test the plugins in Travis
    The plugin functionality it is only tested in the minimal checks.

---
+++ ci/test_plugin_support.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+Script to test the plugin support.
+
+This script assumes that a package created with the default Pysteps plugin template
+(and using the default values) is installed.
+
+https://github.com/aperezhortal/cookiecutter-pysteps-plugin
+
+"""
+
+from pysteps import io
+
+assert hasattr(io.importers, "import_abc_yyy")
+assert hasattr(io.importers, "import_abc_zzz")
+
+assert "abc_yyy" in io.interface._importer_methods
+assert "abc_zzz" in io.interface._importer_methods
+
+from pysteps.io.importers import import_abc_yyy
+from pysteps.io.importers import import_abc_zzz
+
+import_abc_yyy("filename")
+import_abc_zzz("filename")
