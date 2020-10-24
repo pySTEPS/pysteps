77efdacf9c5160c68aa85cdab21b92da4bb4d45a
Andres Perez Hortal <aperezhortal@gmail.com>
2020-10-24 11:54:40 -0400

    Update tests for plugins

--- ci/test_plugin_support.py
+++ ci/test_plugin_support.py
@@ -12,14 +12,16 @@ https://github.com/pySTEPS/cookiecutter-pysteps-plugin
 
 from pysteps import io
 
+print("Testing plugin support: ", end="")
+assert hasattr(io.importers, "import_abc_xxx")
 assert hasattr(io.importers, "import_abc_yyy")
-assert hasattr(io.importers, "import_abc_zzz")
 
+assert "abc_xxx" in io.interface._importer_methods
 assert "abc_yyy" in io.interface._importer_methods
-assert "abc_zzz" in io.interface._importer_methods
 
-from pysteps.io.importers import import_abc_yyy
-from pysteps.io.importers import import_abc_zzz
+from pysteps.io.importers import import_abc_xxx, import_abc_yyy
 
+import_abc_xxx("filename")
 import_abc_yyy("filename")
-import_abc_zzz("filename")
+
+print("PASSED")
