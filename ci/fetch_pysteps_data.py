e0349da46adcb27a8f981fa884fd3e595f7031e0
Andres Perez Hortal <andresperezcba@gmail.com>
2020-11-22 15:18:47 -0500

    Fix DATA path

--- ci/fetch_pysteps_data.py
+++ ci/fetch_pysteps_data.py
@@ -3,17 +3,17 @@
 Script used to install the pysteps data in a test environment and set a pystepsrc
 configuration file that points to that data.
 
-The test data is downloaded in the `PYSTEPS_DATA_DIR` environmental variable.
+The test data is downloaded in the `PYSTEPS_DATA_PATH` environmental variable.
 
 After this script is run, the `PYSTEPSRC` environmental variable should be set to
-PYSTEPSRC=$PYSTEPS_DATA_DIR/pystepsrc for pysteps to use that configuration file.
+PYSTEPSRC=$PYSTEPS_DATA_PATH/pystepsrc for pysteps to use that configuration file.
 """
 
 import os
 
 from pysteps.datasets import create_default_pystepsrc, download_pysteps_data
 
-tox_test_data_dir = os.environ["PYSTEPS_DATA_DIR"]
+tox_test_data_dir = os.environ["PYSTEPS_DATA_PATH"]
 
 download_pysteps_data(tox_test_data_dir, force=True)
 
