b58d8066185219aaae2efbbae60bd0a6a07a1849
Carlos Velasco <carlos.velasco@bom.gov.au>
2021-08-10 08:29:57 +0000

    Add blend_optical_flow

--- pysteps/blending/steps.py
+++ pysteps/blending/steps.py
@@ -2,12 +2,12 @@
 pysteps.blending.steps
 ======================
 
-Implementation of the STEPS stochastic blending method as described in  
+Implementation of the STEPS stochastic blending method as described in
 :cite:`BPS2006`.
 
 .. autosummary::
     :toctree: ../generated/
-    
+
     calculate_ratios
     calculate_weights
     blend_cascades
@@ -21,7 +21,7 @@ import numpy as np
 
 def calculate_ratios(correlations):
     """Calculate explain variance ratios from correlation.
-    
+
     Parameters
     ----------
     Array of shape [component, scale_level, ...]
@@ -43,14 +43,14 @@ def calculate_ratios(correlations):
 
 def calculate_weights(correlations):
     """Calculate blending weights for STEPS blending from correlation.
-    
+
     Parameters
     ----------
     correlations : array-like
       Array of shape [component, scale_level, ...]
       containing correlation (skills) for each component, scale level,
       and optionally along [y, x] dimensions.
-    
+
     Returns
     -------
     weights : array-like
@@ -59,7 +59,7 @@ def calculate_weights(correlations):
       each original component plus noise, scale level, ...
     """
     # correlations: [component, scale, ...]
-    # calculate weights for each source     
+    # calculate weights for each source
     ratios = calculate_ratios(correlations)
     # ratios: [component, scale, ...]
     total_ratios = np.sum(ratios, axis=0)
@@ -67,16 +67,16 @@ def calculate_weights(correlations):
     weights = correlations * np.sqrt(ratios/total_ratios)
     # weights: [component, scale, ...]
     # calculate noise weights
-    total_square_weights = np.sum(np.square(weights),axis=0)
+    total_square_weights = np.sum(np.square(weights), axis=0)
     # total_square_weights: [scale,...]
     noise_weight = np.sqrt(1.0 - total_square_weights)
-    weights = np.concatenate((weights, noise_weight[None,...]), axis=0)
+    weights = np.concatenate((weights, noise_weight[None, ...]), axis=0)
     return weights
 
 
-def blend_cascades(cascades_norm,weights):
+def blend_cascades(cascades_norm, weights):
     """Calculate blended cascades using STEPS weights.
-    
+
     Parameters
     ----------
     cascades_norm : array-like
@@ -87,34 +87,34 @@ def blend_cascades(cascades_norm,weights):
       pysteps.blending.utils.stack_cascades
 
     weights : array-like
-      An array of shape [number_components + 1, scale_level, ...] 
+      An array of shape [number_components + 1, scale_level, ...]
       containing the weights to be used in this rutine
       for each component plus noise, scale level, ...
       obtained by calling a method implemented in
       pysteps.blending.steps.calculate_weights
-    
+
     Returns
     -------
     combined_cascade : array-like
       An array of shape [scale_level, ...] 
-      containing the weighted combination of cascades from multiple 
+      containing the weighted combination of cascades from multiple
       componented to be used in STEPS blending.
     """
 
-    #cascade_norm component, scales, ....
-    #weights component, scales, ....
+    # cascade_norm component, scales, ....
+    # weights component, scales, ....
     all_c_wn = weights * cascades_norm
-    combined_cascade = np.sum(all_c_wn,axis=0)
-    print(combined_cascade.shape)
+    combined_cascade = np.sum(all_c_wn, axis=0)
+    # print(combined_cascade.shape)
     # combined_cascade [scale, ...]
     return combined_cascade
 
 
-def blend_means_sigmas(means,sigmas,weights):
-    #means component, scales, ....
-    #sigmas component, scales, ....
-    #weights component, scales, ....
-    total_weights = np.sum(weights,axis=0)
+def blend_means_sigmas(means, sigmas, weights):
+    # means component, scales, ....
+    # sigmas component, scales, ....
+    # weights component, scales, ....
+    total_weights = np.sum(weights, axis=0)
     factors = weights / total_weights
     diff_dims = factors.ndim - means.ndim
     if diff_dims:
@@ -124,21 +124,17 @@ def blend_means_sigmas(means,sigmas,weights):
     if diff_dims:
         for i in range(diff_dims):
             sigmas = np.expand_dims(sigmas, axis=sigmas.ndim)
-    weighted_means = np.sum((factors * means),axis=0)
-    weighted_sigmas = np.sum((factors * sigmas),axis=0)
+    weighted_means = np.sum((factors * means), axis=0)
+    weighted_sigmas = np.sum((factors * sigmas), axis=0)
 
     # to do: substract covarainces to weigthed sigmas
-  
+
     return weighted_means, weighted_sigmas
 
-    
-def recompose_cascade(w_cascade, w_mean,w_sigma):
+
+def recompose_cascade(w_cascade, w_mean, w_sigma):
     renorm = (w_cascade * w_sigma) + w_mean
     # print(renorm.shape)
-    out = np.sum(renorm,axis=0)
+    out = np.sum(renorm, axis=0)
     # print(out.shape)
     return out
-
-
-
-
--- pysteps/blending/utils.py
+++ pysteps/blending/utils.py
@@ -16,7 +16,7 @@ import numpy as np
 
 def stack_cascades(R_d, donorm=True):
     """Stack the given cascades into a larger array.
-    
+
     Parameters
     ----------
     R_d : list
@@ -24,7 +24,7 @@ def stack_cascades(R_d, donorm=True):
       pysteps.cascade.decomposition.
     donorm : bool
       If True, normalize the cascade levels before stacking.
-    
+
     Returns
     -------
     out : tuple
@@ -35,13 +35,13 @@ def stack_cascades(R_d, donorm=True):
     R_c = []
     mu_c = []
     sigma_c = []
-   
+
     for cascade in R_d:
         R_ = []
         R_i = cascade["cascade_levels"]
         n_levels = R_i.shape[0]
-        mu_ = np.ones(n_levels)* 0.0
-        sigma_ = np.ones(n_levels) *1.0
+        mu_ = np.ones(n_levels) * 0.0
+        sigma_ = np.ones(n_levels) * 1.0
         if donorm:
             mu_ = np.asarray(cascade["means"])
             sigma_ = np.asarray(cascade["stds"])
@@ -50,5 +50,61 @@ def stack_cascades(R_d, donorm=True):
             R_.append(R__)
         R_c.append(np.stack(R_))
         mu_c.append(mu_)
-        sigma_c.append(sigma_)    
-    return np.stack(R_c),np.stack(mu_c),np.stack(sigma_c)
+        sigma_c.append(sigma_)
+    return np.stack(R_c), np.stack(mu_c), np.stack(sigma_c)
+
+
+def blend_optical_flows(flows, weights):
+    """Combine advection fields using given weights.
+
+    Parameters
+    ----------
+    flows : array-like
+      A stack of multiple advenction fields having shape
+      (S, 2, m, n), where flows[N, :, :, :] contains the motion vectors
+      for source N.
+      Advection fields for each source can be obtanined by
+      calling any of the methods implemented in
+      pysteps.motion and then stack all together
+    weights : array-like
+      An array of shape [number_sources]
+      containing the weights to be used to combine
+      the advection fields of each source.
+      weights are modified to make their sum equal to one.
+    Returns
+    -------
+    out: ndarray_
+        Return the blended advection field having shape
+        (2, m, n), where out[0, :, :] contains the x-components of
+        the blended motion vectors and out[1, :, :] contains the y-components.
+        The velocities are in units of pixels / timestep.
+    """
+
+    # check inputs
+    if isinstance(flows, (list, tuple)):
+        flows = np.stack(flows)
+
+    if isinstance(weights, (list, tuple)):
+        weights = np.asarray(weights)
+
+    # check weights dimensions match number of sources
+    num_sources = flows.shape[0]
+    num_weights = weights.shape[0]
+
+    if num_weights != num_sources:
+        raise ValueError(
+            "dimension mismatch between flows and weights.\n"
+            "weights dimension must match the number of flows.\n"
+            f"number of flows={num_sources}, number of weights={num_weights}"
+        )
+    # normalize weigths
+    weights = weights / np.sum(weights)
+
+    # flows dimension sources, 2, m, n
+    # weights dimension sources
+    # move source axis to last to allow broadcasting
+    all_c_wn = weights * np.moveaxis(flows, 0, -1)
+    # sum uses last axis
+    combined_flows = np.sum(all_c_wn, axis=-1)
+    # combined_flows [2, m, n]
+    return combined_flows
