6e3db59a8a21999a8548b8e5de34ecb9ca14030e
RubenImhoff <r.o.imhoff@live.nl>
2021-08-16 11:20:10 +0200

    Add skill score tests and an interface

---
+++ pysteps/blending/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+"""Methods for blending NWP model(s) with nowcasts."""
+
+from pysteps.blending.interface import get_method
---
+++ pysteps/blending/interface.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+pysteps.blending.interface
+==========================
+
+Interface for the blending module. It returns a callable function for computing
+blended nowcasts with NWP models.
+
+.. autosummary::
+    :toctree: ../generated/
+
+    get_method
+"""
+
+from pysteps.blending import steps
+
+_blending_methods = dict()
+_blending_methods["steps"] = steps.forecast
+
+
+def get_method(name):
+    """Return a callable function for computing nowcasts.
+
+    Description:
+    Return a callable function for computing deterministic or ensemble
+    precipitation nowcasts.
+
+    Implemented methods:
+
+    +-----------------+-------------------------------------------------------+
+    |     Name        |              Description                              |
+    +=================+=======================================================+
+    +-----------------+-------------------------------------------------------+
+    |  steps          | the STEPS stochastic nowcasting blending method       |
+    |                 | described in :cite:`Seed2003`, :cite:`BPS2006` and    |
+    |                 | :cite:`SPN2013`. The blending weights approach        |
+    |                 | currently follows :cite:`BPS2006`.                    |
+    +-----------------+-------------------------------------------------------+
+    """
+    if isinstance(name, str):
+        name = name.lower()
+    else:
+        raise TypeError(
+            "Only strings supported for the method's names.\n"
+            + "Available names:"
+            + str(list(_blending_methods.keys()))
+        ) from None
+
+    try:
+        return _blending_methods[name]
+    except KeyError:
+        raise ValueError(
+            "Unknown blending method {}\n".format(name)
+            + "The available methods are:"
+            + str(list(_blending_methods.keys()))
+        ) from None
