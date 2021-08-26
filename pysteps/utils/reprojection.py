10d2856442a19d00958cec5e91c5f0c43cbbc5ea
Wout Dewettinck <wout.dewettinck@ugent.be>
2021-08-26 16:28:02 +0200

    Added compatibility for three-dimensional xarrays

--- pysteps/utils/reprojection.py
+++ pysteps/utils/reprojection.py
@@ -5,11 +5,12 @@ import numpy as np
 
 
 def reprojection(R_src, R_dst):
+
     src_crs = R_src.attrs["projection"]
     x_src = R_src.x
     y_src = R_src.y
-    x1_src = x_src.x1
-    y2_src = y_src.y2
+    x1_src = np.min(x_src)
+    y2_src = np.max(y_src)
     xpixelsize_src = R_src.attrs["xpixelsize"]
     ypixelsize_src = R_src.attrs["ypixelsize"]
     src_transform = A.translation(x1_src, y2_src) * A.scale(
@@ -19,31 +20,33 @@ def reprojection(R_src, R_dst):
     dst_crs = R_dst.attrs["projection"]
     x_dst = R_dst.x
     y_dst = R_dst.y
-    x1_dst = x_dst.x1
-    y2_dst = y_dst.y2
+    x1_dst = np.min(x_dst)
+    y2_dst = np.max(y_dst)
     xpixelsize_dst = R_dst.attrs["xpixelsize"]
     ypixelsize_dst = R_dst.attrs["ypixelsize"]
     dst_transform = A.translation(x1_dst, y2_dst) * A.scale(
         xpixelsize_dst, -ypixelsize_dst
     )
 
-    R_rprj = np.zeros_like(R_dst[:])
+    R_rprj = np.zeros((R_src.shape[0], R_dst.shape[-2], R_dst.shape[-1]))
 
-    reproject(
-        R_src[:],
-        R_rprj,
-        src_transform=src_transform,
-        src_crs=src_crs,
-        dst_transform=dst_transform,
-        dst_crs=dst_crs,
-        resampling=Resampling.nearest,
-        dst_nodata=np.nan,
-    )
+    for i in range(R_src.shape[0]):
+        reproject(
+            R_src[i, :, :],
+            R_rprj[i, :, :],
+            src_transform=src_transform,
+            src_crs=src_crs,
+            dst_transform=dst_transform,
+            dst_crs=dst_crs,
+            resampling=Resampling.nearest,
+            dst_nodata=np.nan,
+        )
 
     R_rprj = xr.DataArray(
         data=R_rprj,
-        dims=("y", "x"),
+        dims=("t", "y", "x"),
         coords=dict(
+            t=("t", R_src.coords["t"].data),
             x=("x", R_dst.coords["x"].data),
             y=("y", R_dst.coords["y"].data),
         ),
