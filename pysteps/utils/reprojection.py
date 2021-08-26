7cde0a63854cfbcdc9c6ac1207b472d4b4bd59d2
Wout Dewettinck <wout.dewettinck@ugent.be>
2021-08-26 16:40:18 +0200

    Add commentary to reprojection util

--- pysteps/utils/reprojection.py
+++ pysteps/utils/reprojection.py
@@ -5,7 +5,28 @@ import numpy as np
 
 
 def reprojection(R_src, R_dst):
+    """Reprojects precipitation fields to the domain of another precipiation
+    field.
 
+    Parameters
+    ----------
+    R_src: xarray
+        Three-dimensional xarray with dimensions (t, x, y) containing a
+        time series of precipitation fields. These precipitaiton fields
+        will be reprojected.
+    R_dst: xarray
+        Xarray containing a precipitation field or a time series of precipitation
+        fields. The xarray R_src will be reprojected to the domain of R_dst.
+
+    Returns
+    -------
+    R_rprj: xarray
+        Three-dimensional xarray with dimensions (t, x, y) containing the
+        precipitation fields of R_src, but reprojected to the domain of
+        R_dst.
+    """
+
+    # Extract the grid info from R_src
     src_crs = R_src.attrs["projection"]
     x_src = R_src.x
     y_src = R_src.y
@@ -17,6 +38,7 @@ def reprojection(R_src, R_dst):
         xpixelsize_src, -ypixelsize_src
     )
 
+    # Extract the grid info from R_dst
     dst_crs = R_dst.attrs["projection"]
     x_dst = R_dst.x
     y_dst = R_dst.y
@@ -28,8 +50,11 @@ def reprojection(R_src, R_dst):
         xpixelsize_dst, -ypixelsize_dst
     )
 
+    # Initialise the reprojected (x)array
     R_rprj = np.zeros((R_src.shape[0], R_dst.shape[-2], R_dst.shape[-1]))
 
+    # For every timestep, reproject the precipitation field of R_src to
+    # the domain of R_dst
     for i in range(R_src.shape[0]):
         reproject(
             R_src[i, :, :],
@@ -42,6 +67,7 @@ def reprojection(R_src, R_dst):
             dst_nodata=np.nan,
         )
 
+    # Assign the necessary attributes from R_src and R_dst to R_rprj
     R_rprj = xr.DataArray(
         data=R_rprj,
         dims=("t", "y", "x"),
