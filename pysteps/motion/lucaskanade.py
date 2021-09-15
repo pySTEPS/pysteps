fbeaf1e3b704de2864118e125cf9fa7b2a3971d9
ned <daniele.nerini@meteoswiss.ch>
2021-09-15 12:16:02 +0200

    More cleanup

--- pysteps/motion/lucaskanade.py
+++ pysteps/motion/lucaskanade.py
@@ -26,7 +26,6 @@ import numpy as np
 from numpy.ma.core import MaskedArray
 
 from pysteps.decorators import check_input_frames
-from pysteps.exceptions import MissingOptionalDependency
 
 from pysteps import utils, feature
 from pysteps.tracking.lucaskanade import track_features
@@ -132,7 +131,7 @@ def dense_lucaskanade(
         :py:func:`pysteps.utils.cleansing.detect_outliers`.
 
     k_outlier: int or None, optional
-        The number of nearest neighbours used to localize the outlier detection.
+        The number of nearest neighbors used to localize the outlier detection.
         If set to None, it employs all the data points (global detection).
         See the documentation of
         :py:func:`pysteps.utils.cleansing.detect_outliers`.
@@ -141,7 +140,7 @@ def dense_lucaskanade(
         The size of the structuring element kernel in pixels. This is used to
         perform a binary morphological opening on the input fields in order to
         filter isolated echoes due to clutter. If set to zero, the filtering
-        is not perfomed.
+        is not performed.
         See the documentation of
         :py:func:`pysteps.utils.images.morph_opening`.
 
@@ -149,7 +148,7 @@ def dense_lucaskanade(
         The scale declustering parameter in pixels used to reduce the number of
         redundant sparse vectors before the interpolation.
         Sparse vectors within this declustering scale are averaged together.
-        If set to less than 2 pixels, the declustering is not perfomed.
+        If set to less than 2 pixels, the declustering is not performed.
         See the documentation of
         :py:func:`pysteps.utils.cleansing.decluster`.
 
