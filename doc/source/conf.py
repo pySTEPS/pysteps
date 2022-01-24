38dd2b0846939cdf2250df07b285dcdfb9e57497
Daniele Nerini <daniele.nerini@gmail.com>
2022-01-24 16:45:25 +0100

    Ignore NWP decomposition example for now

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -245,4 +245,5 @@ sphinx_gallery_conf = {
     "examples_dirs": "../../examples",  # path to your example scripts
     "gallery_dirs": "auto_examples",  # path where to save gallery generated examples
     "filename_pattern": r"/*\.py",  # Include all the files in the examples dir
+    "ignore_pattern": "test_*"
 }
