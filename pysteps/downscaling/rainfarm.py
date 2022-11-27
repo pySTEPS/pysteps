694e9f5711d645b8a7a352fbd344247af331cd72
Daniele Nerini <daniele.nerini@gmail.com>
2022-11-27 18:57:37 +0100

    Refactor code

--- pysteps/downscaling/rainfarm.py
+++ pysteps/downscaling/rainfarm.py
@@ -20,128 +20,15 @@ downscaled precipitation field by means of Gaussian random fields.
 import warnings
 
 import numpy as np
-from scipy.ndimage import convolve
 from scipy.ndimage import zoom
-from pysteps.utils import spectral
+from scipy.signal import convolve
 
 
-def _log_slope(log_k, log_power_spectrum):
-    lk_min = log_k.min()
-    lk_max = log_k.max()
-    lk_range = lk_max - lk_min
-    lk_min += (1 / 6) * lk_range
-    lk_max -= (1 / 6) * lk_range
-
-    selected = (lk_min <= log_k) & (log_k <= lk_max)
-    lk_sel = log_k[selected]
-    ps_sel = log_power_spectrum[selected]
-    alpha = np.polyfit(lk_sel, ps_sel, 1)[0]
-    alpha = -alpha
-
-    return alpha
-
-
-def _balanced_spatial_average(pr_high_res, tophat=None, orig_res=None, kernel="tophat"):
-
+def _gaussianize(precip):
     """
-    Parameters
-    ----------
-    pr_high_res: array_like
-        Array containing the input precipitation field of shape (m, n).
-    tophat: array_like, optional
-        Array of weights, used only if kernel='tophat'.
-    orig_res: int, optional
-        Original size of the precipitation field, used only if kernel='Gaussian'
-    kernel : string, {None, "Gaussian", "tophat"}
-        The smoothing method.
-
-    Returns
-    -------
-    The smoothened field.
-
-    References
-    ----------
-    :cite:`Terzago2018`
-
+    Gaussianize field using rank ordering as in :cite:`DOnofrio2014`.
     """
-
-    indmask = ~np.isfinite(pr_high_res)
-    pr_high_res[indmask] = 0.0
-
-    if kernel == "tophat":
-        ones = np.ones_like(pr_high_res)
-        return convolve(pr_high_res, tophat) / convolve(ones, tophat)
-
-    elif kernel == "Gaussian":
-        ns = np.shape(pr_high_res)[1]
-        sdim = (ns / orig_res) / 2
-        mask = np.zeros([ns, ns])
-        for i in range(ns):
-            for j in range(ns):
-
-                kx = i
-                ky = j
-                if i > (ns / 2):
-                    kx = i - ns
-                if j > (ns / 2):
-                    ky = j - ns
-                r2 = kx * kx + ky * ky
-                mask[i, j] = np.exp(-(r2 / (sdim * sdim)) / 2)
-
-        fm = np.fft.fft2(mask)
-        pf = np.real(np.fft.ifft2(fm * np.fft.fft2(pr_high_res))) / np.sum(mask)
-        if np.sum(indmask) > 0:
-            pr_high_res[~indmask] = 1
-            pf = pf / (
-                np.real(np.fft.ifft2(fm * np.fft.fft2(pr_high_res)))
-                / np.sum(pr_high_res)
-                / len(fm)
-            )
-
-        pf[indmask] = np.nan
-        return pf
-
-    else:
-        raise RuntimeError(
-            'the kernel value does not exist, choose from this list {None, "Gaussian", "tophat"}'
-        )
-
-
-def _check_smooth_value(smooth):
-
-    if smooth is None:
-        return 0
-    elif smooth == "Gaussian":
-        return 1
-    elif smooth == "tophat":
-        return 2
-    else:
-        raise RuntimeError(
-            'the smooth value does not exist, choose from this list {None, "Gaussian", "tophat"}'
-        )
-
-
-def gaussianize(precip):
-    """
-    Gaussianize field using rank ordering.
-
-    Parameters
-    ----------
-    precip: array_like
-        Array containing the input field to be Gaussianized.
-
-    Returns
-    -------
-    precip_gaussianize: array_like
-        The Gaussianized field with the same shape as the input field.
-
-    References
-    ----------
-    :cite:`DOnofrio2014`
-
-    """
-    m = np.shape(precip)[0]
-    n = np.shape(precip)[1]
+    m, n = np.shape(precip)
     nn = m * n
     ii = np.argsort(precip.reshape(nn))
     precip_gaussianize = np.zeros(nn)
@@ -150,10 +37,87 @@ def gaussianize(precip):
     sd = np.std(precip_gaussianize)
     if sd == 0:
         sd = 1
+    return precip_gaussianize / sd
 
-    precip_gaussianize /= sd
 
-    return precip_gaussianize
+def _compute_freq_array(array, ds_factor=1):
+    freq_i = np.fft.fftfreq(array.shape[0] * ds_factor, d=1 / ds_factor)
+    freq_j = np.fft.fftfreq(array.shape[1] * ds_factor, d=1 / ds_factor)
+    freq_sqr = freq_i[:, None] ** 2 + freq_j[None, :] ** 2
+    return np.sqrt(freq_sqr)
+
+
+def _log_slope(log_k, log_power_spectrum):
+    lk_min = log_k.min()
+    lk_max = log_k.max()
+    lk_range = lk_max - lk_min
+    lk_min += (1 / 6) * lk_range
+    lk_max -= (1 / 6) * lk_range
+    selected = (lk_min <= log_k) & (log_k <= lk_max)
+    lk_sel = log_k[selected]
+    ps_sel = log_power_spectrum[selected]
+    alpha = np.polyfit(lk_sel, ps_sel, 1)[0]
+    alpha = -alpha
+    return alpha
+
+
+def _estimate_alpha(array, k):
+    fp = np.fft.fft2(array)
+    fp_abs = abs(fp)
+    log_power_spectrum = np.log(fp_abs**2)
+    valid = (k != 0) & np.isfinite(log_power_spectrum)
+    alpha = _log_slope(np.log(k[valid]), log_power_spectrum[valid])
+    return alpha
+
+
+def _apply_spectral_fusion(array_low, array_high):
+    # TODO: implement same as in
+    # https://github.com/jhardenberg/RainFARM.jl/blob/master/src/rf/mergespec_spaceonly.jl
+    # https://github.com/jhardenberg/rainfarmr/blob/master/R/smoothconv.R
+    raise NotImplementedError
+
+
+def _compute_kernel_radius(ds_factor):
+    return int(round(ds_factor / np.sqrt(np.pi)))
+
+
+def _make_tophat_kernel(ds_factor):
+    """Compute 2d uniform (tophat) kernel"""
+    radius = _compute_kernel_radius(ds_factor)
+    (mx, my) = np.mgrid[-radius : radius + 0.01, -radius : radius + 0.01]
+    tophat = ((mx**2 + my**2) <= radius**2).astype(float)
+    return tophat / tophat.sum()
+
+
+def _make_gaussian_kernel(ds_factor):
+    """
+    Compute 2d gaussian kernel
+    ref: https://github.com/scipy/scipy/blob/de80faf9d3480b9dbb9b888568b64499e0e70c19/scipy/ndimage/_filters.py#L179
+    the smoothing sigma has width half a large pixel
+    """
+    radius = _compute_kernel_radius(ds_factor)
+    sigma = ds_factor / 2
+    sigma2 = sigma * sigma
+    x = np.arange(-radius, radius + 1)
+    kern1d = np.exp(-0.5 / sigma2 * x**2)
+    kern2d = np.outer(kern1d, kern1d)
+    return kern2d / kern2d.sum()
+
+
+def _balanced_spatial_average(array, kernel):
+    array = array.copy()
+    mask_valid = np.isfinite(array)
+    array[~mask_valid] = 0.0
+    array_conv = convolve(array, kernel, mode="warp")
+    array_conv /= convolve(mask_valid, kernel, mode="warp")
+    array_conv[~mask_valid] = np.nan
+    return array_conv
+
+
+_make_kernel = dict()
+_make_kernel["gaussian"] = _make_gaussian_kernel
+_make_kernel["tophat"] = _make_tophat_kernel
+_make_kernel["uniform"] = _make_tophat_kernel
 
 
 def downscale(
@@ -162,12 +126,12 @@ def downscale(
     alpha=None,
     threshold=None,
     return_alpha=False,
-    smooth="Gaussian",
+    kernel_type="gaussian",
     spectral_fusion=False,
 ):
     """
-    Downscale a rainfall field by increasing its spatial resolution by
-    a positive integer factor.
+    Downscale a rainfall field by increasing its spatial resolution by a positive
+    integer factor.
 
     Parameters
     ----------
@@ -185,12 +149,12 @@ def downscale(
         Set all values lower than the threshold to zero.
     return_alpha: bool, optional
         Whether to return the estimated spectral slope ``alpha``.
-    smooth: str, {None, "Gaussian", "tophat"}
+    kernel_type: {"gaussian", "uniform", "tophat"}
         The name of the smoothing operator.
 
     Returns
     -------
-    r: array_like
+    precip_highres: ndarray
         Array of shape (m * ds_factor, n * ds_factor) containing
         the downscaled field.
     alpha: float
@@ -208,86 +172,50 @@ def downscale(
     :cite:`Rebora2006`
 
     """
-    type_smooth = _check_smooth_value(smooth)
-    orig_res = np.shape(precip)[1]
 
     if spectral_fusion:
-        precip_bis = gaussianize(precip)
+        precip_transformed = _gaussianize(precip)
     else:
-        precip_bis = precip
+        precip_transformed = precip
 
-    ki = np.fft.fftfreq(precip_bis.shape[0])
-    kj = np.fft.fftfreq(precip_bis.shape[1])
-    k_sqr = ki[:, None] ** 2 + kj[None, :] ** 2
-    k = np.sqrt(k_sqr)
-
-    ki_ds = np.fft.fftfreq(precip_bis.shape[0] * ds_factor, d=1 / ds_factor)
-    kj_ds = np.fft.fftfreq(precip_bis.shape[1] * ds_factor, d=1 / ds_factor)
-    k_ds_sqr = ki_ds[:, None] ** 2 + kj_ds[None, :] ** 2
-    k_ds = np.sqrt(k_ds_sqr)
+    freq_array = _compute_freq_array(precip_transformed)
+    freq_array_highres = _compute_freq_array(precip_transformed, ds_factor)
 
     if alpha is None:
-        fp = np.fft.fft2(precip_bis)
-        fp_abs = abs(fp)
-        log_power_spectrum = np.log(fp_abs**2)
-        valid = (k != 0) & np.isfinite(log_power_spectrum)
-        alpha = _log_slope(np.log(k[valid]), log_power_spectrum[valid])
+        alpha = _estimate_alpha(precip_transformed, freq_array)
 
-    fg = np.exp(complex(0, 1) * 2 * np.pi * np.random.rand(*k_ds.shape))
+    white_noise_field = np.random.rand(*freq_array_highres.shape)
+    white_noise_field_complex = np.exp(complex(0, 1) * 2 * np.pi * white_noise_field)
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        fg *= np.sqrt(k_ds_sqr ** (-alpha / 2))
-    fg[0, 0] = 0
+        noise_field_complex = white_noise_field_complex * np.sqrt(
+            freq_array_highres**-alpha
+        )
+    noise_field_complex[0, 0] = 0
+    noise_field = np.fft.ifft2(noise_field_complex).real
+    noise_field /= noise_field.std()
+    noise_field = np.exp(noise_field)
 
     if spectral_fusion:
-        fp = np.fft.fft2(precip_bis)
-        kmax = int(precip_bis.shape[0] / 2)
-        nx = fg.shape[0]
-        Pk0 = spectral.rapsd(precip_bis)[kmax - 1]
-        gk0 = spectral.rapsd(np.fft.ifft2(fg).real / np.fft.ifft2(fg).real.std())[
-            kmax - 1
-        ]
-        fg *= Pk0 / gk0
-        fg[0:kmax, 0:kmax] = fp[0:kmax, 0:kmax]
-        fg[nx - kmax :, 0:kmax] = fp[-kmax:, 0:kmax]
-        fg[0:kmax, nx - kmax :] = fp[0:kmax, -kmax:]
-        fg[nx - kmax :, nx - kmax :] = fp[-kmax:, -kmax:]
+        noise_field = _apply_spectral_fusion(precip_transformed, noise_field)
 
-    g = np.fft.ifft2(fg).real
-    g /= g.std()
-    r = np.exp(g)
-
-    P_u = np.repeat(np.repeat(precip, ds_factor, axis=0), ds_factor, axis=1)
-    rad = int(round(ds_factor / np.sqrt(np.pi)))
-    (mx, my) = np.mgrid[-rad : rad + 0.01, -rad : rad + 0.01]
-    tophat = ((mx**2 + my**2) <= rad**2).astype(float)
-    tophat /= tophat.sum()
-
-    if type_smooth == 0:
-
-        P_agg = precip
-        r_agg = zoom(r, 1 / ds_factor, order=1)
-        factor = np.repeat(
-            np.repeat(P_agg / r_agg, ds_factor, axis=0), ds_factor, axis=1
+    try:
+        kernel = _make_kernel[kernel_type](ds_factor)
+    except KeyError:
+        raise ValueError(
+            f"kernel type '{kernel_type}' is invalid, "
+            f"available kernels: {list(_make_kernel)}"
         )
-        r *= factor
 
-    elif type_smooth == 1:
-
-        P_agg = _balanced_spatial_average(P_u, orig_res=orig_res, kernel="Gaussian")
-        r_agg = _balanced_spatial_average(r, orig_res=orig_res, kernel="Gaussian")
-        r *= P_agg / r_agg
-
-    elif type_smooth == 2:
-
-        P_agg = _balanced_spatial_average(P_u, tophat=tophat, kernel="tophat")
-        r_agg = _balanced_spatial_average(r, tophat=tophat, kernel="tophat")
-        r *= P_agg / r_agg
+    precip_zoomed = zoom(precip, ds_factor, order=1)
+    precip_smoothed = _balanced_spatial_average(precip_zoomed, kernel)
+    noise_smoothed = _balanced_spatial_average(noise_field, kernel)
+    precip_highres = noise_field * precip_smoothed / noise_smoothed
 
     if threshold is not None:
-        r[r < threshold] = 0
+        precip_highres[precip_highres < threshold] = 0
 
     if return_alpha:
-        return r, alpha
+        return precip_highres, alpha
 
-    return r
+    return precip_highres
