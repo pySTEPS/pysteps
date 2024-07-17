8b92e71bed50e72e5f91c65696eadcce0c2591db
Mats Veldhuizen <mats.veldhuizen@knmi.nl>
2024-07-17 16:27:51 +0200

    import directly to xarray and rewrite utils_transformation to work with it

--- pysteps/utils/conversion.py
+++ pysteps/utils/conversion.py
@@ -14,6 +14,9 @@ Methods for converting physical units.
 """
 
 import warnings
+
+import xarray as xr
+
 from . import transformation
 
 # TODO: This should not be done. Instead fix the code so that it doesn't
@@ -22,18 +25,53 @@ from . import transformation
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 
-def to_rainrate(R, metadata, zr_a=None, zr_b=None):
+def cf_parameters_from_unit(unit: str) -> tuple[str, dict[str, str | None]]:
+    if unit == "mm/h":
+        var_name = "precip_intensity"
+        var_standard_name = None
+        var_long_name = "instantaneous precipitation rate"
+        var_unit = "mm/h"
+    elif unit == "mm":
+        var_name = "precip_accum"
+        var_standard_name = None
+        var_long_name = "accumulated precipitation"
+        var_unit = "mm"
+    elif unit == "dBZ":
+        var_name = "reflectivity"
+        var_long_name = "equivalent reflectivity factor"
+        var_standard_name = "equivalent_reflectivity_factor"
+        var_unit = "dBZ"
+    else:
+        raise ValueError(f"unknown unit {unit}")
+
+    return var_name, {
+        "standard_name": var_standard_name,
+        "long_name": var_long_name,
+        "units": var_unit,
+    }
+
+
+def _change_unit(dataset: xr.Dataset, precip_var: str, new_unit: str) -> xr.Dataset:
+    new_var, new_attrs = cf_parameters_from_unit(new_unit)
+    dataset = dataset.rename_vars({precip_var: new_var})
+    dataset.attrs["precip_var"] = new_var
+
+    dataset[new_var].attrs = {
+        **dataset[new_var].attrs,
+        **new_attrs,
+    }
+
+    return dataset
+
+
+def to_rainrate(dataset: xr.Dataset, zr_a=None, zr_b=None):
     """
     Convert to rain rate [mm/h].
 
     Parameters
     ----------
-    R: array-like
-        Array of any shape to be (back-)transformed.
-    metadata: dict
-        Metadata dictionary containing the accutime, transform, unit, threshold
-        and zerovalue attributes as described in the documentation of
-        :py:mod:`pysteps.io.importers`.
+    dataset: Dataset
+        Dataset to be (back-)transformed.
 
         Additionally, in case of conversion to/from reflectivity units, the
         zr_a and zr_b attributes are also required,
@@ -45,46 +83,46 @@ def to_rainrate(R, metadata, zr_a=None, zr_b=None):
 
     Returns
     -------
-    R: array-like
-        Array of any shape containing the converted units.
-    metadata: dict
-        The metadata with updated attributes.
+    dataset: Dataset
+        Dataset containing the converted units.
     """
 
-    R = R.copy()
-    metadata = metadata.copy()
+    dataset = dataset.copy(deep=True)
+    precip_var = dataset.attrs["precip_var"]
+    metadata = dataset[precip_var].attrs
+    precip_data = dataset[precip_var].values
 
     if metadata["transform"] is not None:
         if metadata["transform"] == "dB":
-            R, metadata = transformation.dB_transform(R, metadata, inverse=True)
+            dataset = transformation.dB_transform(dataset, inverse=True)
 
         elif metadata["transform"] in ["BoxCox", "log"]:
-            R, metadata = transformation.boxcox_transform(R, metadata, inverse=True)
+            dataset = transformation.boxcox_transform(dataset, inverse=True)
 
         elif metadata["transform"] == "NQT":
-            R, metadata = transformation.NQ_transform(R, metadata, inverse=True)
+            dataset = transformation.NQ_transform(dataset, inverse=True)
 
         elif metadata["transform"] == "sqrt":
-            R, metadata = transformation.sqrt_transform(R, metadata, inverse=True)
+            dataset = transformation.sqrt_transform(dataset, inverse=True)
 
         else:
-            raise ValueError("Unknown transformation %s" % metadata["transform"])
+            raise ValueError(f'Unknown transformation {metadata["transform"]}')
 
-    if metadata["unit"] == "mm/h":
+    if metadata["units"] == "mm/h":
         pass
 
-    elif metadata["unit"] == "mm":
+    elif metadata["units"] == "mm":
         threshold = metadata["threshold"]  # convert the threshold, too
         zerovalue = metadata["zerovalue"]  # convert the zerovalue, too
 
-        R = R / float(metadata["accutime"]) * 60.0
+        precip_data = precip_data / float(metadata["accutime"]) * 60.0
         threshold = threshold / float(metadata["accutime"]) * 60.0
         zerovalue = zerovalue / float(metadata["accutime"]) * 60.0
 
         metadata["threshold"] = threshold
         metadata["zerovalue"] = zerovalue
 
-    elif metadata["unit"] == "dBZ":
+    elif metadata["units"] == "dBZ":
         threshold = metadata["threshold"]  # convert the threshold, too
         zerovalue = metadata["zerovalue"]  # convert the zerovalue, too
 
@@ -93,7 +131,7 @@ def to_rainrate(R, metadata, zr_a=None, zr_b=None):
             zr_a = metadata.get("zr_a", 200.0)  # default to Marshall–Palmer
         if zr_b is None:
             zr_b = metadata.get("zr_b", 1.6)  # default to Marshall–Palmer
-        R = (R / zr_a) ** (1.0 / zr_b)
+        precip_data = (precip_data / zr_a) ** (1.0 / zr_b)
         threshold = (threshold / zr_a) ** (1.0 / zr_b)
         zerovalue = (zerovalue / zr_a) ** (1.0 / zr_b)
 
@@ -104,13 +142,12 @@ def to_rainrate(R, metadata, zr_a=None, zr_b=None):
 
     else:
         raise ValueError(
-            "Cannot convert unit %s and transform %s to mm/h"
-            % (metadata["unit"], metadata["transform"])
+            f'Cannot convert unit {metadata["units"]} and transform {metadata["transform"]} to mm/h'
         )
 
-    metadata["unit"] = "mm/h"
-
-    return R, metadata
+    dataset[precip_var].data[:] = precip_data
+    dataset = _change_unit(dataset, precip_var, "mm/h")
+    return dataset
 
 
 def to_raindepth(R, metadata, zr_a=None, zr_b=None):
