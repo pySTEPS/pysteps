66ef437d4cc9dba18ba5dd7d742f88e455fe1fea
Mats Veldhuizen <mats.veldhuizen@knmi.nl>
2024-07-18 17:38:13 +0200

    make dimension.py xarray compatible

--- pysteps/tests/test_utils_dimension.py
+++ pysteps/tests/test_utils_dimension.py
@@ -4,63 +4,89 @@ import datetime as dt
 
 import numpy as np
 import pytest
+import xarray as xr
 from numpy.testing import assert_array_equal
 from pytest import raises
 
+from pysteps.converters import convert_to_xarray_dataset
 from pysteps.utils import dimension
+
+fillvalues_metadata = {
+    "x1": 0,
+    "x2": 4,
+    "y1": 0,
+    "y2": 4,
+    "xpixelsize": 1,
+    "ypixelsize": 1,
+    "zerovalue": 0,
+    "yorigin": "lower",
+    "unit": "mm/h",
+    "transform": None,
+    "accutime": 5,
+    "threshold": 1.0,
+    "projection": "+proj=stere +lat_0=90 +lon_0=0.0 +lat_ts=60.0 +a=6378.137 +b=6356.752 +x_0=0 +y_0=0",
+    "zr_a": 200,
+    "zr_b": 1.6,
+    "cartesian_unit": "km",
+    "institution": "KNMI",
+}
 
 test_data_not_trim = (
     # "data, window_size, axis, method, expected"
-    (np.arange(6), 2, 0, "mean", np.array([0.5, 2.5, 4.5])),
+    (
+        np.arange(12).reshape(2, 6),
+        2,
+        "x",
+        "mean",
+        np.array([[0.5, 2.5, 4.5], [6.5, 8.5, 10.5]]),
+    ),
     (
         np.arange(4 * 6).reshape(4, 6),
         (2, 3),
-        (0, 1),
+        ("y", "x"),
         "sum",
         np.array([[24, 42], [96, 114]]),
     ),
     (
         np.arange(4 * 6).reshape(4, 6),
         (2, 2),
-        (0, 1),
+        ("y", "x"),
         "sum",
         np.array([[14, 22, 30], [62, 70, 78]]),
     ),
     (
         np.arange(4 * 6).reshape(4, 6),
         2,
-        (0, 1),
+        ("y", "x"),
         "sum",
         np.array([[14, 22, 30], [62, 70, 78]]),
     ),
     (
         np.arange(4 * 6).reshape(4, 6),
         (2, 3),
-        (0, 1),
+        ("y", "x"),
         "mean",
         np.array([[4.0, 7.0], [16.0, 19.0]]),
     ),
     (
         np.arange(4 * 6).reshape(4, 6),
         (2, 2),
-        (0, 1),
+        ("y", "x"),
         "mean",
         np.array([[3.5, 5.5, 7.5], [15.5, 17.5, 19.5]]),
     ),
     (
         np.arange(4 * 6).reshape(4, 6),
         2,
-        (0, 1),
+        ("y", "x"),
         "mean",
         np.array([[3.5, 5.5, 7.5], [15.5, 17.5, 19.5]]),
     ),
 )
 
 
-@pytest.mark.parametrize(
-    "data, window_size, axis, method, expected", test_data_not_trim
-)
-def test_aggregate_fields(data, window_size, axis, method, expected):
+@pytest.mark.parametrize("data, window_size, dim, method, expected", test_data_not_trim)
+def test_aggregate_fields(data, window_size, dim, method, expected):
     """
     Test the aggregate_fields function.
     The windows size must divide exactly the data dimensions.
@@ -68,23 +94,25 @@ def test_aggregate_fields(data, window_size, axis, method, expected):
     windows size does not divide the data dimensions.
     The length of each dimension should be larger than 2.
     """
+    dataset = convert_to_xarray_dataset(data, None, fillvalues_metadata)
 
-    assert_array_equal(
-        dimension.aggregate_fields(data, window_size, axis=axis, method=method),
-        expected,
-    )
+    actual = dimension.aggregate_fields(dataset, window_size, dim=dim, method=method)
+    assert_array_equal(actual["precip_intensity"].values, expected)
 
     # Test the trimming capabilities.
-    data = np.pad(data, (0, 1))
-    assert_array_equal(
-        dimension.aggregate_fields(
-            data, window_size, axis=axis, method=method, trim=True
-        ),
-        expected,
+    if np.ndim(window_size) == 0:
+        data = np.pad(data, ((0, 0), (0, 1)))
+    else:
+        data = np.pad(data, (0, 1))
+    dataset = convert_to_xarray_dataset(data, None, fillvalues_metadata)
+
+    actual = dimension.aggregate_fields(
+        dataset, window_size, dim=dim, method=method, trim=True
     )
+    assert_array_equal(actual["precip_intensity"].values, expected)
 
     with raises(ValueError):
-        dimension.aggregate_fields(data, window_size, axis=axis, method=method)
+        dimension.aggregate_fields(dataset, window_size, dim=dim, method=method)
 
 
 def test_aggregate_fields_errors():
@@ -93,80 +121,124 @@ def test_aggregate_fields_errors():
     function.
     """
     data = np.arange(4 * 6).reshape(4, 6)
+    dataset = convert_to_xarray_dataset(data, None, fillvalues_metadata)
 
     with raises(ValueError):
-        dimension.aggregate_fields(data, -1, axis=0)
+        dimension.aggregate_fields(dataset, -1, dim="y")
     with raises(ValueError):
-        dimension.aggregate_fields(data, 0, axis=0)
+        dimension.aggregate_fields(dataset, 0, dim="y")
     with raises(ValueError):
-        dimension.aggregate_fields(data, 1, method="invalid")
+        dimension.aggregate_fields(dataset, 1, method="invalid")
 
     with raises(TypeError):
-        dimension.aggregate_fields(data, (1, 1), axis=0)
+        dimension.aggregate_fields(dataset, (1, 1), dim="y")
 
 
 # aggregate_fields_time
-timestamps = [dt.datetime.now() + dt.timedelta(minutes=t) for t in range(10)]
+now = dt.datetime.now()
+timestamps = [now + dt.timedelta(minutes=t) for t in range(10)]
 test_data = [
     (
-        np.ones((10, 1, 1)),
+        np.ones((2, 2)),
         {"unit": "mm/h", "timestamps": timestamps},
         2,
         False,
-        np.ones((5, 1, 1)),
+        np.ones((5, 2, 2)),
     ),
     (
-        np.ones((10, 1, 1)),
+        np.ones((2, 2)),
         {"unit": "mm", "timestamps": timestamps},
         2,
         False,
-        2 * np.ones((5, 1, 1)),
+        2 * np.ones((5, 2, 2)),
     ),
 ]
 
 
 @pytest.mark.parametrize(
-    "R, metadata, time_window_min, ignore_nan, expected", test_data
+    "data, metadata, time_window_min, ignore_nan, expected", test_data
 )
-def test_aggregate_fields_time(R, metadata, time_window_min, ignore_nan, expected):
+def test_aggregate_fields_time(data, metadata, time_window_min, ignore_nan, expected):
     """Test the aggregate_fields_time."""
+    dataset_ref = convert_to_xarray_dataset(
+        data, None, {**fillvalues_metadata, **metadata}
+    )
+    datasets = []
+    for timestamp in metadata["timestamps"]:
+        dataset_ = dataset_ref.copy(deep=True)
+        dataset_ = dataset_.expand_dims(dim="time", axis=0)
+        dataset_ = dataset_.assign_coords(time=("time", [timestamp]))
+        datasets.append(dataset_)
+
+    dataset = xr.concat(datasets, dim="time")
     assert_array_equal(
-        dimension.aggregate_fields_time(R, metadata, time_window_min, ignore_nan)[0],
+        dimension.aggregate_fields_time(dataset, time_window_min, ignore_nan)[
+            "precip_intensity" if metadata["unit"] == "mm/h" else "precip_accum"
+        ].values,
         expected,
     )
 
 
 # aggregate_fields_space
-test_data = [
+test_data_space = [
     (
-        np.ones((1, 10, 10)),
-        {"unit": "mm/h", "xpixelsize": 1, "ypixelsize": 1},
+        np.ones((10, 10)),
+        {
+            "unit": "mm/h",
+            "x1": 0,
+            "x2": 10,
+            "y1": 0,
+            "y2": 10,
+            "xpixelsize": 1,
+            "ypixelsize": 1,
+        },
         2,
         False,
-        np.ones((1, 5, 5)),
+        np.ones((5, 5)),
     ),
     (
-        np.ones((1, 10, 10)),
-        {"unit": "mm", "xpixelsize": 1, "ypixelsize": 1},
+        np.ones((10, 10)),
+        {
+            "unit": "mm",
+            "x1": 0,
+            "x2": 10,
+            "y1": 0,
+            "y2": 10,
+            "xpixelsize": 1,
+            "ypixelsize": 1,
+        },
         2,
         False,
-        np.ones((1, 5, 5)),
+        np.ones((5, 5)),
     ),
     (
-        np.ones((1, 10, 10)),
-        {"unit": "mm/h", "xpixelsize": 1, "ypixelsize": 2},
-        (2, 4),
+        np.ones((10, 10)),
+        {
+            "unit": "mm/h",
+            "x1": 0,
+            "x2": 10,
+            "y1": 0,
+            "y2": 20,
+            "xpixelsize": 1,
+            "ypixelsize": 2,
+        },
+        (4, 2),
         False,
-        np.ones((1, 5, 5)),
+        np.ones((5, 5)),
     ),
 ]
 
 
-@pytest.mark.parametrize("R, metadata, space_window, ignore_nan, expected", test_data)
-def test_aggregate_fields_space(R, metadata, space_window, ignore_nan, expected):
+@pytest.mark.parametrize(
+    "data, metadata, space_window, ignore_nan, expected", test_data_space
+)
+def test_aggregate_fields_space(data, metadata, space_window, ignore_nan, expected):
     """Test the aggregate_fields_space."""
+    dataset = convert_to_xarray_dataset(data, None, {**fillvalues_metadata, **metadata})
     assert_array_equal(
-        dimension.aggregate_fields_space(R, metadata, space_window, ignore_nan)[0],
+        dimension.aggregate_fields_space(dataset, space_window, ignore_nan)[
+            "precip_intensity" if metadata["unit"] == "mm/h" else "precip_accum"
+        ].values,
         expected,
     )
 
@@ -174,59 +246,35 @@ def test_aggregate_fields_space(R, metadata, space_window, ignore_nan, expected)
 # clip_domain
 R = np.zeros((4, 4))
 R[:2, :] = 1
-test_data = [
+test_data_clip_domain = [
     (
         R,
-        {
-            "x1": 0,
-            "x2": 4,
-            "y1": 0,
-            "y2": 4,
-            "xpixelsize": 1,
-            "ypixelsize": 1,
-            "zerovalue": 0,
-            "yorigin": "upper",
-        },
+        {"yorigin": "lower"},
         None,
         R,
     ),
     (
         R,
-        {
-            "x1": 0,
-            "x2": 4,
-            "y1": 0,
-            "y2": 4,
-            "xpixelsize": 1,
-            "ypixelsize": 1,
-            "zerovalue": 0,
-            "yorigin": "lower",
-        },
+        {"yorigin": "lower"},
         (2, 4, 2, 4),
         np.zeros((2, 2)),
     ),
     (
         R,
-        {
-            "x1": 0,
-            "x2": 4,
-            "y1": 0,
-            "y2": 4,
-            "xpixelsize": 1,
-            "ypixelsize": 1,
-            "zerovalue": 0,
-            "yorigin": "upper",
-        },
+        {"yorigin": "upper"},
         (2, 4, 2, 4),
         np.ones((2, 2)),
     ),
 ]
 
 
-@pytest.mark.parametrize("R, metadata, extent, expected", test_data)
+@pytest.mark.parametrize("R, metadata, extent, expected", test_data_clip_domain)
 def test_clip_domain(R, metadata, extent, expected):
     """Test the clip_domain."""
-    assert_array_equal(dimension.clip_domain(R, metadata, extent)[0], expected)
+    dataset = convert_to_xarray_dataset(R, None, {**fillvalues_metadata, **metadata})
+    assert_array_equal(
+        dimension.clip_domain(dataset, extent)["precip_intensity"].values, expected
+    )
 
 
 # square_domain
