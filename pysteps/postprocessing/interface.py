11abb0d689909397011466a97ef3031bae7abeb7
joeycasey87 <139773651+joeycasey87@users.noreply.github.com>
2024-07-25 12:06:15 +0200

    Update interface.py

--- pysteps/postprocessing/interface.py
+++ pysteps/postprocessing/interface.py
@@ -16,18 +16,19 @@ import importlib
 
 from pkg_resources import iter_entry_points
 
-from pysteps import postprocessing
-from pysteps.postprocessing import postprocessors
+import postprocessing
+from postprocessing import diagnostics
 from pprint import pprint
 
-_postprocessor_methods = dict()
+_diagnostics_methods = dict()
 
-def discover_postprocessors():
+
+def discover_diagnostics():
     """
-    Search for installed postprocessors plugins in the entrypoint 'pysteps.plugins.postprocessors'
+    Search for installed diagnostics plugins in the entrypoint 'pysteps.plugins.diagnostics'
 
-    The postprocessors found are added to the `pysteps.postprocessing.interface_postprocessor_methods`
-    dictionary containing the available postprocessors.
+    The diagnostics found are added to the `pysteps.postprocessing.interface_diagnostics_methods`
+    dictionary containing the available diagnostics.
     """
 
     # The pkg resources needs to be reloaded to detect new packages installed during
@@ -37,81 +38,92 @@ def discover_postprocessors():
 
     importlib.reload(pkg_resources)
 
-    for entry_point in pkg_resources.iter_entry_points(group='pysteps.plugins.postprocessors', name=None):
-        _postprocessor = entry_point.load()
+    for entry_point in pkg_resources.iter_entry_points(
+            group="pysteps.plugins.diagnostics", name=None
+    ):
+        _diagnostics = entry_point.load()
 
-        postprocessor_function_name = _postprocessor.__name__
-        postprocessor_short_name = postprocessor_function_name.replace("postprocess_", "")
+        diagnostics_function_name = _diagnostics.__name__
+        diagnostics_short_name = diagnostics_function_name.replace(
+            "diagnostics_", ""
+        )
 
-        _postprocess_kws = getattr(_postprocessor, "postprocess_kws", dict())
-        _postprocessor = postprocess_import(**_postprocess_kws)(_postprocessor)
-        if postprocessor_short_name not in _postprocessor_methods:
-            _postprocessor_methods[postprocessor_short_name] = _postprocessor
+        _diagnostics_kws = getattr(_diagnostics, "diagnostics_kws", dict())
+        if diagnostics_short_name not in _diagnostics_methods:
+            _diagnostics_methods[diagnostics_short_name] = _diagnostics
         else:
             RuntimeWarning(
-                f"The postprocessor identifier '{postprocessor_short_name}' is already available in "
-                "'pysteps.postprocessing.interface_postprocessor_methods'.\n"
+                f"The diagnostics identifier '{diagnostics_short_name}' is already available in "
+                "'pysteps.postprocessing.interface_diagnostics_methods'.\n"
                 f"Skipping {entry_point.module_name}:{'.'.join(entry_point.attrs)}"
             )
 
-        if hasattr(postprocessors, postprocessor_function_name):
+        if hasattr(diagnostics, diagnostics_function_name):
             RuntimeWarning(
-                f"The postprocessor function '{postprocessor_function_name}' is already an attribute"
-                "of 'pysteps.postprocessing.postprocessors'.\n"
+                f"The diagnostics function '{diagnostics_function_name}' is already an attribute"
+                "of 'pysteps.postprocessing.diagnostics'.\n"
                 f"Skipping {entry_point.module_name}:{'.'.join(entry_point.attrs)}"
             )
         else:
-            setattr(postprocessors, postprocessor_function_name, _postprocessor)
+            setattr(diagnostics, diagnostics_function_name, _diagnostics)
 
-def postprocessors_info():
-    """Print all the available postprocessors."""
 
-    # Postprocessors available in the 'postprocessing.postprocessors' module
-    available_postprocessors = [
-        attr for attr in dir(postprocessing.postprocessors) if attr.startswith("postprocess_")
+def diagnostics_info():
+    """Print all the available diagnostics."""
+
+    # diagnostics available in the 'postprocessing.diagnostics' module
+    available_diagnostics = [
+        attr
+        for attr in dir(postprocessing.diagnostics)
+        if attr.startswith("diagnostics")
     ]
 
-    print("\nPostprocessors available in the pysteps.postprocessing.postprocessors module")
-    pprint(available_postprocessors)
+    print(
+        "\ndiagnostics available in the pysteps.postprocessing.diagnostics module"
+    )
+    pprint(available_diagnostics)
 
-    # Postprocessors declared in the pysteps.postprocessing.get_method interface
-    postprocessors_in_the_interface = [
-        f.__name__ for f in postprocessing.interface._postprocessor_methods.values()
+    # diagnostics declared in the pysteps.postprocessing.get_method interface
+    diagnostics_in_the_interface = [
+        f for f in list(postprocessing.interface._diagnostics_methods.keys())
     ]
 
-    print("\nPostprocessors available in the pysteps.postprocessing.get_method interface")
+    print(
+        "\ndiagnostics available in the pysteps.postprocessing.get_method interface"
+    )
     pprint(
         [
             (short_name, f.__name__)
-            for short_name, f in postprocessing.interface._postprocessor_methods.items()
+            for short_name, f in postprocessing.interface._diagnostics_methods.items()
         ]
     )
 
-    # Let's use sets to find out if there are postprocessors present in the postprocessor module
-    # but not declared in the interface, and viceversa.
-    available_postprocessors = set(available_postprocessors)
-    postprocessors_in_the_interface = set(postprocessors_in_the_interface)
+    # Let's use sets to find out if there are diagnostics present in the diagnostics module
+    # but not declared in the interface, and vice versa.
+    available_diagnostics = set(available_diagnostics)
+    diagnostics_in_the_interface = set(diagnostics_in_the_interface)
 
-    difference = available_postprocessors ^ postprocessors_in_the_interface
+    difference = available_diagnostics ^ diagnostics_in_the_interface
     if len(difference) > 0:
         print("\nIMPORTANT:")
-        _diff = available_postprocessors - postprocessors_in_the_interface
+        _diff = available_diagnostics - diagnostics_in_the_interface
         if len(_diff) > 0:
             print(
-                "\nIMPORTANT:\nThe following postprocessors are available in pysteps.postprocessing.postprocessors module "
-                "but not in the pysteps.postprocessing.get_method interface"
+                "\nIMPORTANT:\nThe following diagnostics are available in pysteps.postprocessing.diagnostics "
+                "module but not in the pysteps.postprocessing.get_method interface"
             )
             pprint(_diff)
-        _diff = postprocessors_in_the_interface - available_postprocessors
+        _diff = diagnostics_in_the_interface - available_diagnostics
         if len(_diff) > 0:
             print(
                 "\nWARNING:\n"
-                "The following postprocessors are available in the pysteps.postprocessing.get_method "
-                "interface but not in the pysteps.postprocessing.postprocessors module"
+                "The following diagnostics are available in the pysteps.postprocessing.get_method "
+                "interface but not in the pysteps.postprocessing.diagnostics module"
             )
             pprint(_diff)
 
-    return available_postprocessors, postprocessors_in_the_interface
+    return available_diagnostics, diagnostics_in_the_interface
+
 
 def get_method(name, method_type):
     """
@@ -123,7 +135,7 @@ def get_method(name, method_type):
     name: str
         Name of the method. The available options are:\n
 
-        Postprocessors:
+        diagnostics:
 
         .. tabularcolumns:: |p{2cm}|L|
 
@@ -131,7 +143,15 @@ def get_method(name, method_type):
         |     Name    |              Description                              |
         +=============+=======================================================+
 
-    method_type: {'postprocessor'}
+        Diagnostic diagnostics:
+
+        .. tabularcolumns:: |p{2cm}|L|
+
+        +-------------+-------------------------------------------------------+
+        |     Name    |              Description                              |
+        +=============+=======================================================+
+
+    method_type: {'diagnostics', diagnostic_diagnostics}
             Type of the method (see tables above).
 
     """
@@ -142,7 +162,7 @@ def get_method(name, method_type):
         raise TypeError(
             "Only strings supported for for the method_type"
             + " argument\n"
-            + "The available types are: 'postprocessor'"
+            + "The available types are: 'diagnostics'"
         ) from None
 
     if isinstance(name, str):
@@ -150,16 +170,16 @@ def get_method(name, method_type):
     else:
         raise TypeError(
             "Only strings supported for the method's names.\n"
-            + "\nAvailable postprocessors names:"
-            + str(list(_postprocessor_methods.keys()))
+            + "\nAvailable diagnostics names:"
+            + str(list(_diagnostics_methods.keys()))
         ) from None
 
-    if method_type == "postprocessor":
-        methods_dict = _postprocessor_methods
+    if method_type == "diagnostics":
+        methods_dict = _diagnostics_methods
     else:
         raise ValueError(
-            "Unknown method type {}\n".format(name)
-            + "The available types are: 'postprocessor'"
+            "Unknown method type {}\n".format(method_type)
+            + "The available types are: 'diagnostics'"
         ) from None
 
     try:
