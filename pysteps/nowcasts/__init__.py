7b4b17fcc9a845467ecc16109eaefa6159c2782e
Loic <loicfaleu123@gmail.com>
2024-07-31 10:00:24 +0200

    handling dgmr_plugin import error at the __init__.py

--- pysteps/nowcasts/__init__.py
+++ pysteps/nowcasts/__init__.py
@@ -1,11 +1,16 @@
 """Implementations of deterministic and ensemble nowcasting methods."""
 
 from pysteps.nowcasts.interface import *
+discover_nowcasts()
 
 try:
     from dgmr_module_plugin import dgmr
 except ImportError:
-    dgmr=None
+    print("Error: DGMR plugin is required but not installed.")
+    print("Please install the plugin to use this feature.")
+    print("You can install it using the following command:")
+    print("pip install git+https://github.com/LoicKemajou/dgmr_plugin.git")
+
 
 
 
