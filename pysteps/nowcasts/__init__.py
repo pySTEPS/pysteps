724b3cf590083e69487e7e3d2ba096ef257902c9
Loic <loicfaleu123@gmail.com>
2024-08-04 22:07:12 +0200

    update in __init__ file

--- pysteps/nowcasts/__init__.py
+++ pysteps/nowcasts/__init__.py
@@ -1,15 +1,8 @@
 """Implementations of deterministic and ensemble nowcasting methods."""
 
 from pysteps.nowcasts.interface import *
-discover_nowcasts()
 
-try:
-    from dgmr_module_plugin import dgmr
-except ImportError:
-    print("Error: DGMR plugin is required but not installed.")
-    print("Please install the plugin to use this feature.")
-    print("You can install it using the following command:")
-    print("pip install git+https://github.com/LoicKemajou/dgmr_plugin.git")
+discover_nowcasts()
 
 
 
