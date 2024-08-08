08de5984ea3c5ace456c74c0d55d2837173fdc0c
Loickemajou <88109303+Loickemajou@users.noreply.github.com>
2024-08-08 11:19:12 +0200

    adding the dgmr model to be directly imported by pysteps.nowcasts

--- pysteps/nowcasts/__init__.py
+++ pysteps/nowcasts/__init__.py
@@ -1,3 +1,8 @@
 """Implementations of deterministic and ensemble nowcasting methods."""
 
 from pysteps.nowcasts.interface import get_method
+
+try:
+    from dgmr_module_plugin import dgmr
+except ImportError:
+    dgmr=None
