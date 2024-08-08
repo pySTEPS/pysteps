24d6c411a3c363d6429b9e4229c93091fc96e2a5
Loickemajou <88109303+Loickemajou@users.noreply.github.com>
2024-08-08 13:46:54 +0200

    reformatted __init__.py

--- pysteps/nowcasts/__init__.py
+++ pysteps/nowcasts/__init__.py
@@ -1,8 +1,8 @@
 """Implementations of deterministic and ensemble nowcasting methods."""
 
-from pysteps.nowcasts.interface import get_method
+from pysteps.nowcasts.interface import *
 
 try:
     from dgmr_module_plugin import dgmr
 except ImportError:
-    dgmr=None
+    dgmr = None
