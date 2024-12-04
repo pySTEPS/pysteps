e3325854dae006b1e78eb43a6d2203f2b0b71560
Daniele Nerini <daniele.nerini@gmail.com>
2024-12-04 20:45:15 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ requirements = [
 
 setup(
     name="pysteps",
-    version="1.12.0",
+    version="1.13.0",
     author="PySteps developers",
     packages=find_packages(),
     license="LICENSE",
