f340085fc15d8ddd89abc402f299d92a7092dd60
Lesley De Cruz <lesley.decruz@meteo.be>
2025-02-27 00:16:42 +0100

    Fix plugin tests to use new cookiecutter template; try out importer and diagnostic plugin.

--- pysteps/tests/test_plugins_support.py
+++ pysteps/tests/test_plugins_support.py
@@ -17,29 +17,41 @@ from cookiecutter.main import cookiecutter
 PLUGIN_TEMPLATE_URL = "https://github.com/pysteps/cookiecutter-pysteps-plugin"
 
 from contextlib import contextmanager
-import pysteps
+from pysteps import io, postprocessing
 
 
-def _check_installed_plugin(import_func_name):
+def _check_installed_importer_plugin(import_func_name):
     # reload the pysteps module to detect the installed plugin
-    pysteps.io.discover_importers()
-    assert hasattr(pysteps.io.importers, import_func_name)
-    assert import_func_name in pysteps.io.interface._importer_methods
-    importer = getattr(pysteps.io.importers, import_func_name)
+    io.discover_importers()
+    print(io.importers_info())
+    import_func_name = import_func_name.replace("importer_", "import_")
+    assert hasattr(io.importers, import_func_name)
+    func_name = import_func_name.replace("import_", "")
+    assert func_name in io.interface._importer_methods
+    importer = getattr(io.importers, import_func_name)
     importer("filename")
 
 
+def _check_installed_diagnostic_plugin(diagnostic_func_name):
+    # reload the pysteps module to detect the installed plugin
+    postprocessing.discover_postprocessors()
+    assert hasattr(postprocessing.diagnostics, diagnostic_func_name)
+    assert diagnostic_func_name in postprocessing.interface._diagnostics_methods
+    diagnostic = getattr(postprocessing.diagnostics, diagnostic_func_name)
+    diagnostic("filename")
+
+
 @contextmanager
-def _create_and_install_plugin(project_name, importer_name):
+def _create_and_install_plugin(project_name, plugin_type):
     with tempfile.TemporaryDirectory() as tmpdirname:
-        print(f"Installing plugin {project_name} providing the {importer_name} module")
+        print(f"Installing plugin {project_name} providing a {plugin_type} module")
         cookiecutter(
             PLUGIN_TEMPLATE_URL,
             no_input=True,
             overwrite_if_exists=True,
             extra_context={
                 "project_name": project_name,
-                "importer_name": importer_name,
+                "plugin_type": plugin_type,
             },
             output_dir=tmpdirname,
         )
@@ -73,9 +85,10 @@ def _uninstall_plugin(project_name):
 
 
 def test_importers_plugins():
-    with _create_and_install_plugin("test_importer_aaa", "importer_aaa"):
-        # The default plugin template appends an _xxx to the importer function.
-        _check_installed_plugin("importer_aaa_xxx")
-        with _create_and_install_plugin("test_importer_bbb", "importer_bbb"):
-            _check_installed_plugin("importer_aaa_xxx")
-            _check_installed_plugin("importer_bbb_xxx")
+    with _create_and_install_plugin("pysteps-importer-institution-fun", "importer"):
+        _check_installed_importer_plugin("importer_institution_fun")
+
+
+def test_diagnostic_plugins():
+    with _create_and_install_plugin("pysteps-diagnostic-fun", "diagnostic"):
+        _check_installed_diagnostic_plugin("diagnostic_fun")
