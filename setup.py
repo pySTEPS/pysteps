d4ac4c02c50121e1e45e80040948ea6f479f8b2e
Ruben Imhoff <31476760+RubenImhoff@users.noreply.github.com>
2025-04-23 15:03:00 +0200

    Fixes to linear and saliency based blending (#466)
    * Fix: ensure linear blending can also handle multiple input timesteps and ensure linear blending can deal with nans.
    
    * add new release
    
    ---------
    
    Co-authored-by: mats-knmi <145579783+mats-knmi@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ requirements = [
 
 setup(
     name="pysteps",
-    version="1.16.0",
+    version="1.17.0",
     author="PySteps developers",
     packages=find_packages(),
     license="LICENSE",
