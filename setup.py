1c3623b955a9358bbb89f4cd8518e665d1f368df
Ruben Imhoff <31476760+RubenImhoff@users.noreply.github.com>
2026-02-27 09:19:12 +0100

    docs: update version to 1.20.0 (#540)

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ requirements = [
 
 setup(
     name="pysteps",
-    version="1.19.1",
+    version="1.20.0",
     author="PySteps developers",
     packages=find_packages(),
     license="LICENSE",
